c9577dd019caab91cc2790f37982fdb4d72d54a3
Michael Soegtrop <msoegtrop@michael-soegtrop.de>
2017-06-11 10:10:05 +0000

    Revert "Fixed bool-lpe release mode issue"
    This reverts commit 483fa860135966bbf39ac5c51a5c6c8a9fbf2db7

--- src/live_effects/lpe-bool.h
+++ src/live_effects/lpe-bool.h
@@ -42,7 +42,7 @@ public:
     inline friend bool_op to_bool_op(bool_op_ex val)
     {
         assert(val <= bool_op_ex_slice);
-        return (bool_op) val;
+        (bool_op) val;
     }
 
 private:
