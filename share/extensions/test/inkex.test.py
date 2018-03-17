4b8c40ef6c313df11aa73a8303bc1bcb8ad28311
Dave Bort <git@dbort.com>
2018-03-17 16:13:44 -0700

    [inkex] Rewrite inkex.errormsg tests to check output.
    The old code, when referring to "Latin 1", seemed to in fact be talking
    about literal (un-escpaed) Unicode characters. Passing actual Latin 1
    characters to errormsg() causes them to be interpreted as UTF-8 and
    produces garbage output.
    
    Also, now that the output is checked programmatically, there's no need
    to increase the verbosity of the test; use unittest.main() like most
    other tests.

--- share/extensions/test/inkex.test.py
+++ share/extensions/test/inkex.test.py
@@ -10,29 +10,60 @@ If you want to help, read the python unittest documentation:
 http://docs.python.org/library/unittest.html
 '''
 
+import StringIO
 import sys
-sys.path.append('..') # this line allows to import the extension code
-
 import unittest
-from inkex import errormsg
 
-class InkexBasicTest(unittest.TestCase):
+sys.path.append('..') # this line allows to import the extension code
+import inkex
 
-    #def setUp(self):
+
+class ErrormsgTest(unittest.TestCase):
+    """Tests for inkex.errormsg()."""
+
+    def setUp(self):
+        """Installs a fake stderr in the inkex module."""
+        self.fake_stderr = StringIO.StringIO()
+        self._real_stderr = inkex.sys.stderr
+        inkex.sys.stderr = self.fake_stderr
+
+    def tearDown(self):
+        """Restores the real stderr in the inkex module."""
+        self.fake_stderr.close()
+        inkex.sys.stderr = self._real_stderr
+
+    def assertOutput(self, expected_bytes):
+        """Tests the bytes written to the fake stderr."""
+        self.assertEqual(self.fake_stderr.getvalue(), expected_bytes)
 
     def test_ascii(self):
-        #Parse ABCabc
-        errormsg('ABCabc')
+        # A string (not the 'unicode' type), containing only 7-bit-clean
+        # ASCII characters.
+        inkex.errormsg('ABCabc')
+        self.assertOutput('ABCabc\n')
 
-    def test_nonunicode_latin1(self):
-        #Parse Àûïàèé
-        errormsg('Àûïàèé')
+    def test_nonunicode_type(self):
+        # A string (not the 'unicode' type), containing literal
+        # non-ASCII Unicode characters.
+        msg = 'Àûïàèé'
+        # Demonstrate that this string contains UTF-8-encoded bytes,
+        # because this text file is encoded as UTF-8.
+        self.assertEqual(
+                msg, '\xc3\x80\xc3\xbb\xc3\xaf\xc3\xa0\xc3\xa8\xc3\xa9')
 
-    def test_unicode_latin1(self):
-        #Parse Àûïàèé (unicode)
-        errormsg(u'Àûïàèé')
+        inkex.errormsg(msg)
+        self.assertOutput('\xc3\x80\xc3\xbb\xc3\xaf\xc3\xa0\xc3\xa8\xc3\xa9\n')
+
+    def test_unicode_type(self):
+        # A 'unicode' string, containing literal non-ASCII Unicode
+        # characters.
+        msg = u'Àûïàèé'
+        # Demonstrate that this string contains Unicode characters, not UTF-8.
+        self.assertEqual(msg, u'\xc0\xfb\xef\xe0\xe8\xe9')
+
+        inkex.errormsg(msg)
+        self.assertOutput('\xc3\x80\xc3\xbb\xc3\xaf\xc3\xa0\xc3\xa8\xc3\xa9\n')
+
 
 if __name__ == '__main__':
-    #unittest.main()
-    suite = unittest.TestLoader().loadTestsFromTestCase(InkexBasicTest)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    unittest.main()
