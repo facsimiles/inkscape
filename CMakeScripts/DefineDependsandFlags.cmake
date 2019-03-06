nullmastermind/inkscape-for-trace

commit f7fc3d3a7f8f51a01cd1dfb261c3e234617e092e

Fix build

CMakeLists.txt
@@ -114,7 +114,7 @@ option(WITH_LIBVISIO "Compile with support of libvisio for Microsoft Visio Diagr
 option(WITH_LIBWPG "Compile with support of libwpg for WordPerfect Graphics" ON)
 option(WITH_NLS "Compile with Native Language Support (using gettext)" ON)
 option(WITH_YAML "Compile with YAML support (enables xverbs)" ON)
-option(WITH_JEMALLOC "Compile with JEMALLOC support" ON)
+option(WITH_JEMALLOC "Compile with JEMALLOC support" OFF)
 
 option(WITH_FUZZ "Compile for fuzzing purpose (use 'make fuzz' only)" OFF)
 mark_as_advanced(WITH_FUZZ)

CMakeScripts/DefineDependsandFlags.cmake
@@ -21,6 +21,7 @@ list(APPEND INKSCAPE_CXX_FLAGS "-std=c++11")
 # Errors for common mistakes
 list(APPEND INKSCAPE_CXX_FLAGS "-Werror=format")                # e.g.: printf("%s", std::string("foo"))
 list(APPEND INKSCAPE_CXX_FLAGS "-Werror=format-security")       # e.g.: printf(variable);
+list(APPEND INKSCAPE_CXX_FLAGS_DEBUG "-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong -grecord-gcc-switches -fasynchronous-unwind-tables -fstack-clash-protection -fcf-protection")
 
 # Define the flags for profiling if desired:
 if(WITH_PROFILING)
@@ -363,6 +364,9 @@ endif()
 list(REMOVE_DUPLICATES INKSCAPE_CXX_FLAGS)
 foreach(flag ${INKSCAPE_CXX_FLAGS})
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}" CACHE STRING "" FORCE)
+endforeach()
+foreach(flag ${INKSCAPE_CXX_FLAGS_DEBUG})
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${flag}" CACHE STRING "" FORCE)
 endforeach()
 
 # Some linkers, like gold, don't find symbols recursively. So we have to link against X11 explicitly

.clang-tidy
@@ -2,5 +2,5 @@
 Checks:          'clang-diagnostic-*,clang-analyzer-*, modernize-deprecated-headers, modernize-loop-convert, modernize-use-override, modernize-redundant-void-arg, modernize-use-emplace, modernize-use-noexcept, modernize-use-bool-literals, modernize-pass-by-value, modernize-use-equals-default, modernize-use-equals-default, modernize-use-equals-delete'
 #, readability-*, bugprone-*'
 WarningsAsErrors: ''
-HeaderFilterRegex: ''
+HeaderFilterRegex: '.*'
 FormatStyle:     'file'

org.inkscape.Inkscape.appdata.xml.in
@@ -51,6 +51,11 @@
         </ul>
       </description>
     </release>
+    <release version="1.0-alpha" date="2019-01-04">
+      <description>
+        <p>This is the first public alpha release of Inkscape 1.0! This package is intended to test against regressions, try new features, and get a taste of the future of Inkscape!</p>
+      </description>
+  </release>
     <release version="0.92.3" date="2018-03-23">
       <description>
         <ul>

po/POTFILES.in
@@ -1,6 +1,6 @@
 # List of source files containing translatable strings.
 # Please keep this file sorted alphabetically.
-# Generated by generate_POTFILES.sh at Tue Feb  5 20:06:57 CET 2019
+# Generated by ./generate_POTFILES.sh at Mon Feb 11 11:51:47 CET 2019
 [encoding: UTF-8]
 org.inkscape.Inkscape.appdata.xml.in
 org.inkscape.Inkscape.desktop.in
@@ -109,6 +109,7 @@ src/gradient-chemistry.cpp
 src/gradient-drag.cpp
 src/helper/gettext.cpp
 src/helper/gettext.h
+src/helper/verb-action.cpp
 src/inkscape-application.cpp
 src/inkscape.cpp
 src/inkview-application.cpp

facsimiles/inkscape

commit 9711d6975106844936b2c6de5995d7a533154d6b

Improve Debug building
- add -O1 to make FORTIFY_SOURCE happy (eliminate compiler warnings)
- canonicalize CMAKE_CXX_FLAGS_DEBUG to avoid unnecessary recompilation

Both issues were introduced with f7fc3d3a7f8f51a01cd1dfb261c3e234617e092e

CMakeLists.txt
@@ -203,6 +203,8 @@ endif()
 include(CMakeScripts/CanonicalizeFlagsVar.cmake)
 canonicalize_flags_var("${CMAKE_CXX_FLAGS}" _new_cxx)
 set(CMAKE_CXX_FLAGS "${_new_cxx}" CACHE STRING "" FORCE)
+canonicalize_flags_var("${CMAKE_CXX_FLAGS_DEBUG}" _new_cxx)
+set(CMAKE_CXX_FLAGS_DEBUG "${_new_cxx}" CACHE STRING "" FORCE)
 # message(FATAL_ERROR "CMAKE_CXX_FLAGS = <${CMAKE_CXX_FLAGS}>")
 
 

CMakeScripts/DefineDependsandFlags.cmake
@@ -23,6 +23,7 @@ list(APPEND INKSCAPE_CXX_FLAGS "-Werror=format")                # e.g.: printf("
 list(APPEND INKSCAPE_CXX_FLAGS "-Werror=format-security")       # e.g.: printf(variable);
 list(APPEND INKSCAPE_CXX_FLAGS_DEBUG "-D_FORTIFY_SOURCE=2")
 if (CMAKE_COMPILER_IS_GNUCC)
+list(APPEND INKSCAPE_CXX_FLAGS_DEBUG "-O1")                     # for _FORTIFY_SOURCE
 list(APPEND INKSCAPE_CXX_FLAGS_DEBUG "-fexceptions -fstack-protector-strong -grecord-gcc-switches -fasynchronous-unwind-tables")
 if(CXX_COMPILER_VERSION VERSION_GREATER 8.0)
 list(APPEND INKSCAPE_CXX_FLAGS_DEBUG "-fstack-clash-protection -fcf-protection")