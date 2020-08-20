c62d4ffd459ba3698ed0995e8f148d43ccb1b7bf
Moazin Khatti <moazinkhatri@gmail.com>
2020-08-20 17:47:53 +0500

    added docs for intersection finding

--- src/livarot/livarot-doxygen.cpp
+++ src/livarot/livarot-doxygen.cpp
@@ -31,7 +31,7 @@
  * After you're done you convert it back to something that Inkscape can understand.
  *
  * Following are the steps you take when you want to use Livarot's features in
- * Inkscape:
+ * %Inkscape:
  *
  * 1. You take a Geom::PathVector or an SPItem and convert it to a Livarot Path
  * object. You can do this by using the methods in src/path/path-util.h. See the
@@ -61,4 +61,150 @@
  * above, you'd need to call Shape::ConvertToForme to extract contours from
  * the directed graph. Basically, it creates a Path object and you can dump
  * an SVG path `d' attribute.
+ *
+ * @section ApproximateLine Approximation by Line Segments
+ *
+ * You start with creating a Path object and adding path descriptions by command
+ * functions such as Path::MoveTo, Path::LineTo, Path::Close, etc. Once you've done
+ * that you use one of Path::Convert, Path::ConvertEvenLines and Path::ConvertWithBackData.
+ * These functions create a line segment approximation for the path descriptions that
+ * were stored.
+ *
+ * @subsection PathConvert Path::Convert
+ *
+ * Simply creates a line segment approximation. All curves get approximated by line
+ * segments while respecting the threshold. The smaller this threshold, the more line
+ * segments there will be. The bigger this threshold, the less line segments there will
+ * be. All sorts of path descriptions that are not line segments get approximated by
+ * line segments. Line segments remain as they were. See the following figures, first one
+ * shows a bigger threshold value and the second one shows a smaller threshold value.
+ *
+ * @image html livarot-images/convert-simple-low.svg
+ * @image html livarot-images/convert-simple-high.svg
+ *
+ * @subsection PathConvertEvenLines Path::ConvertEvenLines
+ *
+ * This is totally identical to Path::Convert except that it breaks up line segments
+ * into further smaller line segments.
+ *
+ * @image html livarot-images/convert-lines.svg
+ *
+ * @subsection PathConvertWithBackData Path::ConvertWithBackData
+ *
+ * It is identical to Path::Convert but with only one difference. It stores something called
+ * "back data". Basically, it stores information about where the point comes from. It stores
+ * two things. Firstly there is "piece" which is the index of the path description that this
+ * point comes from and then there is "time" which is the time value at which if you were to
+ * evaluate the path description you'd get that point. A time value of 0 means the start of
+ * the path description. A time value of 1 means the end of the path description. See the
+ * following figure. The 0 and 1 written are indexes of those path descriptions. There are
+ * two path descriptions, first is the bezier curve (index 0) and the second is the line
+ * segment (index 1).
+ *
+ * @image html livarot-images/convert-back-data.svg
+ *
+ * To see the details of how this splitting into line segments process happens, you can go
+ * into the details of these functions and the functions they further call such as Path::RecCubicTo.
+ *
+ * @section GraphFilling Making Directed Graph
+ *
+ * The following pictures shows the whole process. We start with a path description and approximate it
+ * by line segments. The points of these line segments can store back data as seen earlier. Then, calling
+ * Path::Fill will create a directed graph consisting of vertices and edges that will be stored in a Shape
+ * object. These edges also have back data stored, each edge stores the piece it comes from and the time of
+ * its start point as well as the time of its end point.
+ *
+ * @image html livarot-images/process-flat-to-convert.svg
+ *
+ * @section ConvertShape Removing intersections and reconstruction
+ *
+ * This is probably the most fundamental part of Livarot. I'll describe the algorithm in detail here, as much
+ * as I could understand it. There are lots of things it does such as:
+ * 1. Finding intersections.
+ * 2. Reconstruction of the directed graph.
+ * 3. Saving some data for winding number seeds computation for later.
+ * 4. Remove any doublon edges (edges on top of each other).
+ * 5. Compute winding numbers.
+ * 6. Manipulate edges (keep them same, flip them or remove them) given the fill rule.
+ *
+ * Let me describe the algorithm for each one in great detail now.
+ *
+ * @subsection FindingIntersections Finding self-intersections.
+ *
+ * The algorithm to find self-intersections is known as Bentley Ottoman sweepline algorithm. You'll easily
+ * find the details in literature however I want to give a complete description of the implementation in
+ * livarot as comprehensively as I can.
+ *
+ * The general idea of the sweepline algorithm is that we sweep a horizontal line from top to bottom of the
+ * graph and keep track of the linear order in which the edges intersect with the sweepline. Instead of moving
+ * the sweepline in a continious fashion (which is not possible in a digital world), we move it only at certain
+ * fixed points. These fixed points are the endpoints of the line segments and any possible intersection points
+ * that we detect.
+ *
+ * We keep two data structures for our needs. The first one has to be linear structure that can preserve the
+ * order in which items are stored. A double linked list should suffice for this purpose however we use a double
+ * linked list + an AVL tree for faster searching. Secondly, we keep a priority queue (a min heap), to keep track
+ * of all the intersections that we detect. The priority queue stores these in such a way that the top most and
+ * left most intersection (if there are multiple ones at same y) get popped.
+ *
+ * We start by sorting all the points of the graph top to bottom (and left to right if there are points at the same
+ * y level). After that we place the sweepline at the top most point. To determine which point to go next, we check
+ * both the next point in the list of sorted vertices of the directed graph and the earliest intersection event in
+ * the priority queue. We choose to go with whichever comes first.
+ *
+ * There are certain things that we need to do at each point the sweepline lands on (which can either be an intersection
+ * point that we detected earlier) or an endpoint of the edges. For endpoints, we need to:
+ * 1. See if there are edges that need to be added to the sweepline list (they just started). For each edge that you add,
+ * you must find the right position where it should go in the sweepline list. Once you find that right position, insert
+ * it there and test if this edge intersects with the one on its right and its left in that list. If any intersections
+ * are detected, record them. This testing is done by doing maths that can tell if two line segments intersect.
+ * 2. See if there are edges that need to be removed the sweepline list (they ended here). For each edge that you remove,
+ * unless that edge was either the head or tail of the sweepline list, it'd leave its two neighbors who'd now be side
+ * to side. So you must test if these two neighbors intersect with each other and if they do, record that intersection
+ * event.
+ * Both of these are shown in the figure below.
+ *
+ * @image html livarot-images/sweepline-list-node-add-removal.svg
+ *
+ * When the sweepline lands on an intersection point we need to:
+ * 1. Swap the two edges that intersect. The one on the left becomes the right and the one on the right becomes left.
+ * 2. Detect if the left one has another edge on its left and test if these two intersect with each other.
+ * 3. Detect if the right one has another edge on its right and test if these two intersect with each other.
+ *
+ * This is shown in the figure below.
+ *
+ * @image html livarot-images/sweepline-list-node-intersection.svg
+ *
+ * There is one every important final thing that you need to manage throughout this process and I'm discussing it separately
+ * because it applies to the whole process. Whenver you detect an intersection, you store this intersection with references
+ * to the original nodes that intersect, that's very obvious. But you should also store reference to the intersection event
+ * within the sweepline list node. So each intersection event will have two references, one to the left node and the other
+ * to the right node. Each node will have two references (to interesection events), right and left. The left one will point
+ * to any possible intersection that has the node as a "right" node in the intersection. Similarly, the right one will
+ * point to any intersection that has the node as a "left" node in the intersection. This is shown in the following figure.
+ * Index 0 is LEFT and Index 1 is RIGHT. There are three intersection events stored namely X, Y and Z. Intersection X is
+ * between node A and B. Intersection Y is between node B and C. Intersection Z is between node C and node D. As you can see
+ * in the figure, each intersection event stores a pointer to the left node and a pointer to the right node, these are sweep[0]
+ * and sweep[1] respectively. You can also see that each node stores two pointers namely evt[0] and evt[1]. evt[0] is the
+ * pointer to any intersection event that references this node as its right edge. evt[1] is the pointer to any intersection
+ * event that references this node as its left edge. You can see how these are set for the intersections in the figure.
+ *
+ * @image html livarot-images/sweepline-list-node-event.svg
+ *
+ * The important thing that you need to take care of, is that anytime two nodes who are going to intersect are together or
+ * side by side and it is known that they will intersect at some point, and you have an intersection event for this, if they
+ * switch places or any other node gets inserted between them, that intersection event must be deleted immediately and any
+ * references to it should be removed too. This can happen mainly at a few points:
+ * 1. A node being inserted between two nodes whose intersection has been detected already.
+ * 2. Two nodes whose intersection is being processed and they are to be swapped but the nodes happen to have intersections
+ * with their other neighbors. Say you have nodes A -> B -> C -> D and we have intersection events of BC and CD. When we
+ * process the event of BC, we swap them and sequence becomes A -> C -> B -> D and now CD are no longer side by side, thus
+ * their intersection event should be purged immedately and any references that might point to it too.
+ * The calls to Remove and RemoveEvent you see in the code are exactly for this purpose.
+ *
+ * This is mostly what there is to finding self-intersections.
+ *
+ * @subsection Reconstruction Reconstruction of directed graph
+ *
+ *
  */
