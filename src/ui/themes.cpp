b837b4dfa8e3a6bd020db5ecb117f751aec107b7
Jabier Arraiza <jabier.arraiza@marker.es>
2022-10-08 21:12:35 +0200

    use gtk-icon-pallete to recolor icons

--- src/ui/themes.cpp
+++ src/ui/themes.cpp
@@ -157,9 +157,11 @@ ThemeContext::get_symbolic_colors()
     sp_svg_write_color(colornamederror, sizeof(colornamederror), colorseterror);
     colorsetbase_inverse = colorsetbase ^ 0xffffff00;
     sp_svg_write_color(colornamed_inverse, sizeof(colornamed_inverse), colorsetbase_inverse);
-    css_str += "@define-color warning_color " + Glib::ustring(colornamedwarning) + ";\n";
-    css_str += "@define-color error_color " + Glib::ustring(colornamederror) + ";\n";
-    css_str += "@define-color success_color " + Glib::ustring(colornamedsuccess) + ";\n";
+    css_str = "image{";
+    css_str += "-gtk-icon-palette: warning " + Glib::ustring(colornamedwarning);
+    css_str += ", error " + Glib::ustring(colornamederror);
+    css_str += ", success " + Glib::ustring(colornamedsuccess);
+    css_str += "} \n";
     /* ":not(.rawstyle) > image" works only on images in first level of widget container
     if in the future we use a complex widget with more levels and we dont want to tweak the color
     here, retaining default we can add more lines like ":not(.rawstyle) > > image" 
