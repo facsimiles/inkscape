cd50a797adcfaa10aa23fd23824dd3eca5a659b6
Jabier Arraiza <jabier.arraiza@marker.es>
2023-09-23 11:35:15 +0200

    fixbulid

--- src/ui/dialog/swatches.cpp
+++ src/ui/dialog/swatches.cpp
@@ -32,6 +32,7 @@
 #include <gtkmm/menubutton.h>
 #include <gtkmm/radiobutton.h>
 #include <gtkmm/searchentry.h>
+#include <gtkmm/window.h>
 #include <pangomm/layout.h>
 
 #include "desktop.h"
@@ -560,7 +561,7 @@ void SwatchesPanel::rebuild()
 }
 
 bool SwatchesPanel::load_swatches() {
-    auto window = dynamic_cast<Gtk::Window*>(get_toplevel());
+    auto window = dynamic_cast<Gtk::Window*>(_palette->get_toplevel());
     auto file = choose_palette_file(window);
     auto loaded = false;
     if (load_swatches(file)) {
