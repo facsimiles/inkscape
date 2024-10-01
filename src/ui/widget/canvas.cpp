facsimiles/inkscape

commit 20459d4d7913c4bc0ffa85cbe5b9f8a635057a40

Port CanvasItem hierarchy to CanvasEvents
* Add inspect_event() for type-safely performing a switch statement on
  the dynamic type of an event, like std::visit() for std::variant.
* Remove unused function SPDesktopWidget::event().
* Move CanvasGrid::SignalEvent() into Canvas as per todo, and remove
  the key press/release forwarding which appears unnecessary.

src/ui/widget/canvas.cpp
@@ -23,6 +23,7 @@
 #include "canvas.h"
 
 #include "color.h"          // Background color
+#include "desktop-events.h"
 #include "desktop.h"
 #include "document.h"
 #include "preferences.h"
@@ -43,6 +44,11 @@
 #include "display/control/canvas-item-group.h"
 #include "display/control/snap-indicator.h"
 #include "events/canvas-event.h"
+
+// Hack: Needed for the workaround that closes the command palette.
+#include "ui/dialog/command-palette.h"
+#include "ui/widget/canvas-grid.h"
+#include "widgets/desktop-widget.h"
 
 #include "ui/tools/tool-base.h"      // Default cursor
 
@@ -903,6 +909,16 @@ bool Canvas::on_button_pressed(GtkGestureMultiPress *controller, int n_press, do
     _state = gdkevent->button.state;
     d->last_mouse = Geom::IntPoint(x, y);
 
+    grab_focus();
+
+    // Close the command palette if it loses focus; it can't do it itself since signal_focus() appears to be broken.
+    // Todo: (GTK4) Try to get rid of this using GtkEventControllerFocus.
+    _desktop->getDesktopWidget()->get_canvas_grid()->getCommandPalette()->close();
+
+    if (gdkevent->button.button == 3) {
+        _desktop->getCanvasDrawing()->set_sticky(_state & GDK_SHIFT_MASK);
+    }
+
     // Drag the split view controller.
     if (_split_mode == Inkscape::SplitMode::SPLIT && _hover_direction != Inkscape::SplitDirection::NONE) {
         if (n_press == 1) {
@@ -1390,7 +1406,7 @@ bool CanvasPrivate::emit_event(CanvasEvent &event)
 
         // Propagate the event up the canvas item hierarchy until handled.
         while (item) {
-            if (item->handle_event(event.original())) return true;
+            if (item->handle_event(event)) return true;
             item = item->get_parent();
         }
     }

src/desktop-events.cpp
@@ -52,6 +52,7 @@
 #include "ui/tools/node-tool.h"
 #include "ui/tools/select-tool.h"
 #include "ui/widget/canvas.h"
+#include "ui/widget/events/canvas-event.h"
 
 #include "widgets/desktop-widget.h"
 
@@ -65,8 +66,9 @@ static void init_extended();
 
 /* Root canvas item handler */
 
-bool sp_desktop_root_handler(GdkEvent *event, SPDesktop *desktop)
+bool sp_desktop_root_handler(Inkscape::CanvasEvent const &canvas_event, SPDesktop *desktop)
 {
+    auto event = canvas_event.original();
 #ifdef EVENT_DEBUG
     ui_dump_event(reinterpret_cast<GdkEvent *>(event), "sp_desktop_root_handler");
 #endif
@@ -98,7 +100,13 @@ static SPGuideDragType drag_type = SP_DRAG_NONE;
 // Min distance from anchor to initiate rotation, measured in screenpixels
 #define tol 40.0
 
-bool sp_dt_guide_event(GdkEvent *event, Inkscape::CanvasItemGuideLine *guide_item, SPGuide *guide)
+bool sp_dt_guide_event(Inkscape::CanvasEvent const &canvas_event, Inkscape::CanvasItemGuideLine *guide_item, SPGuide *guide)
+{
+    auto event = canvas_event.original();
+    return sp_dt_guide_event_gdkevent(event, guide_item, guide);
+}
+
+bool sp_dt_guide_event_gdkevent(GdkEvent *event, Inkscape::CanvasItemGuideLine *guide_item, SPGuide *guide)
 {
 #ifdef EVENT_DEBUG
     ui_dump_event(reinterpret_cast<GdkEvent *>(event), "sp_dt_guide_event");

src/desktop-events.h
@@ -19,17 +19,19 @@ class SPGuide;
 
 namespace Inkscape {
 class CanvasItemGuideLine;
+class CanvasEvent;
 }
 
 typedef union _GdkEvent GdkEvent;
 
 /* Item handlers */
 
-bool sp_desktop_root_handler(GdkEvent *event, SPDesktop *desktop);
+bool sp_desktop_root_handler(Inkscape::CanvasEvent const &event, SPDesktop *desktop);
 
 /* Guides */
 
-bool sp_dt_guide_event(GdkEvent *event, Inkscape::CanvasItemGuideLine *guide_item, SPGuide *guide);
+bool sp_dt_guide_event(Inkscape::CanvasEvent const &event, Inkscape::CanvasItemGuideLine *guide_item, SPGuide *guide);
+bool sp_dt_guide_event_gdkevent(GdkEvent *event, Inkscape::CanvasItemGuideLine *guide_item, SPGuide *guide); // Deprecated
 
 #endif // INKSCAPE_DESKTOP_EVENTS_H
 

src/desktop.cpp
@@ -65,6 +65,7 @@
 #include "ui/tools/box3d-tool.h"
 #include "ui/tools/select-tool.h"
 #include "ui/widget/canvas.h"
+#include "ui/widget/events/canvas-event.h"
 
 #include "widgets/desktop-widget.h"
 
@@ -75,7 +76,7 @@
 namespace Inkscape { namespace XML { class Node; }}
 
 // Callback declarations
-static bool _drawing_handler (GdkEvent *event, Inkscape::DrawingItem *item, SPDesktop *desktop);
+static bool _drawing_handler(Inkscape::CanvasEvent const &event, Inkscape::DrawingItem *item, SPDesktop *desktop);
 static void _reconstruction_start(SPDesktop * desktop);
 static void _reconstruction_finish(SPDesktop * desktop);
 
@@ -1411,9 +1412,10 @@ SPDesktop::onDocumentFilenameSet (gchar const* filename)
 /**
  * Calls event handler of current event context.
  */
-static bool
-_drawing_handler (GdkEvent *event, Inkscape::DrawingItem *drawing_item, SPDesktop *desktop)
+static bool _drawing_handler(Inkscape::CanvasEvent const &canvas_event, Inkscape::DrawingItem *drawing_item, SPDesktop *desktop)
 {
+    auto event = canvas_event.original();
+
     if (event->type == GDK_KEY_PRESS && Inkscape::UI::Tools::get_latin_keyval(&event->key) == GDK_KEY_space &&
         desktop->event_context->is_space_panning())
     {

src/display/control/canvas-item-drawing.cpp
@@ -25,6 +25,7 @@
 
 #include "helper/geom.h"
 #include "ui/widget/canvas.h"
+#include "ui/widget/events/canvas-event.h"
 #include "ui/modifiers.h"
 
 namespace Inkscape {
@@ -84,31 +85,26 @@ void CanvasItemDrawing::_update(bool)
 
     _bounds = expandedBy(_drawing->root()->drawbox(), 1); // Avoid aliasing artifacts
 
-    // Todo: This should be managed elsewhere.
     if (_cursor) {
         /* Mess with enter/leave notifiers */
-        DrawingItem *new_drawing_item = _drawing->pick(_c, _delta, _sticky * DrawingItem::PICK_STICKY | _pick_outline * DrawingItem::PICK_OUTLINE);
+        auto new_drawing_item = _drawing->pick(_c, _delta, _sticky * DrawingItem::PICK_STICKY | _pick_outline * DrawingItem::PICK_OUTLINE);
         if (_active_item != new_drawing_item) {
+            // Fixme: These crossing events have no modifier state set.
 
-            GdkEventCrossing ec;
-            ec.window = get_canvas()->get_window()->gobj();
-            ec.send_event = true;
-            ec.subwindow = ec.window;
-            ec.time = GDK_CURRENT_TIME;
-            ec.x = _c.x();
-            ec.y = _c.y();
-
-            /* fixme: Why? */
             if (_active_item) {
-                ec.type = GDK_LEAVE_NOTIFY;
-                _drawing_event_signal.emit((GdkEvent *) &ec, _active_item);
+                auto gdkevent = GdkEventUniqPtr(gdk_event_new(GDK_LEAVE_NOTIFY));
+                auto event = LeaveEvent(std::move(gdkevent), {});
+                _drawing_event_signal.emit(event, _active_item);
             }
 
             _active_item = new_drawing_item;
 
             if (_active_item) {
-                ec.type = GDK_ENTER_NOTIFY;
-                _drawing_event_signal.emit((GdkEvent *) &ec, _active_item);
+                auto gdkevent = GdkEventUniqPtr(gdk_event_new(GDK_ENTER_NOTIFY));
+                gdkevent->crossing.x = _c.x();
+                gdkevent->crossing.y = _c.y();
+                auto event = EnterEvent(std::move(gdkevent), {});
+                _drawing_event_signal.emit(event, _active_item);
             }
         }
     }
@@ -126,12 +122,12 @@ void CanvasItemDrawing::_render(Inkscape::CanvasItemBuffer &buf) const
 /**
  * Handle events directed at the drawing. We first attempt to handle them here.
  */
-bool CanvasItemDrawing::handle_event(GdkEvent *event)
+bool CanvasItemDrawing::handle_event(CanvasEvent const &event)
 {
     bool retval = false;
     
-    switch (event->type) {
-        case GDK_ENTER_NOTIFY:
+    inspect_event(event,
+        [&] (EnterEvent const &event) {
             if (!_cursor) {
                 if (_active_item) {
                     std::cerr << "CanvasItemDrawing::event_handler: cursor entered drawing with an active item!" << std::endl;
@@ -139,69 +135,62 @@ bool CanvasItemDrawing::handle_event(GdkEvent *event)
                 _cursor = true;
 
                 /* TODO ... event -> arena transform? */
-                _c = Geom::Point(event->crossing.x, event->crossing.y);
+                _c = event.eventPos();
 
                 _active_item = _drawing->pick(_c, _drawing->cursorTolerance(), _sticky * DrawingItem::PICK_STICKY | _pick_outline * DrawingItem::PICK_OUTLINE);
                 retval = _drawing_event_signal.emit(event, _active_item);
             }
-            break;
+        },
 
-        case GDK_LEAVE_NOTIFY:
+        [&] (LeaveEvent const &event) {
             if (_cursor) {
                 retval = _drawing_event_signal.emit(event, _active_item);
                 _active_item = nullptr;
                 _cursor = false;
             }
-            break;
+        },
 
-        case GDK_MOTION_NOTIFY:
-        {
+        [&] (MotionEvent const &event) {
             /* TODO ... event -> arena transform? */
-            _c = Geom::Point(event->motion.x, event->motion.y);
+            _c = event.eventPos();
 
             auto new_drawing_item = _drawing->pick(_c, _drawing->cursorTolerance(), _sticky * DrawingItem::PICK_STICKY | _pick_outline * DrawingItem::PICK_OUTLINE);
             if (_active_item != new_drawing_item) {
 
-                GdkEventCrossing ec;
-                ec.window = event->motion.window;
-                ec.send_event = event->motion.send_event;
-                ec.subwindow = event->motion.window;
-                ec.time = event->motion.time;
-                ec.x = event->motion.x;
-                ec.y = event->motion.y;
-
                 /* fixme: What is wrong? */
                 if (_active_item) {
-                    ec.type = GDK_LEAVE_NOTIFY;
-                    retval = _drawing_event_signal.emit((GdkEvent *) &ec, _active_item);
+                    auto gdkevent = GdkEventUniqPtr(gdk_event_new(GDK_LEAVE_NOTIFY));
+                    auto event = LeaveEvent(std::move(gdkevent), {});
+                    retval = _drawing_event_signal.emit(event, _active_item);
                 }
 
                 _active_item = new_drawing_item;
 
                 if (_active_item) {
-                    ec.type = GDK_ENTER_NOTIFY;
-                    retval = _drawing_event_signal.emit((GdkEvent *) &ec, _active_item);
+                    auto gdkevent = GdkEventUniqPtr(gdk_event_new(GDK_ENTER_NOTIFY));
+                    gdkevent->crossing.x = event.eventX();
+                    gdkevent->crossing.y = event.eventY();
+                    auto event = EnterEvent(std::move(gdkevent), {});
+                    retval = _drawing_event_signal.emit(event, _active_item);
                 }
             }
             retval = retval || _drawing_event_signal.emit(event, _active_item);
-            break;
-        }
+        },
 
-        case GDK_SCROLL:
-        {
-            if (Modifiers::Modifier::get(Modifiers::Type::CANVAS_ZOOM)->active(event->scroll.state)) {
+        [&] (ScrollEvent const &event) {
+            if (Modifiers::Modifier::get(Modifiers::Type::CANVAS_ZOOM)->active(event.modifiers())) {
                 /* Zoom is emitted by the canvas as well, ignore here */
-                return false;
+                retval = false;
+                return;
             }
             retval = _drawing_event_signal.emit(event, _active_item);
-            break;
-        }
+        },
 
-        default:
-            /* Just send event */
+        [&] (CanvasEvent const &event) {
+            // Just send event.
             retval = _drawing_event_signal.emit(event, _active_item);
-            break;
-    }
+        }
+    );
 
     return retval;
 }

src/display/control/canvas-item-drawing.h
@@ -44,12 +44,12 @@ public:
     Inkscape::DrawingItem *get_active() { return _active_item; }
 
     // Events
-    bool handle_event(GdkEvent *event) override;
+    bool handle_event(CanvasEvent const &event) override;
     void set_sticky(bool sticky) { _sticky = sticky; }
     void set_pick_outline(bool pick_outline) { _pick_outline = pick_outline; }
 
     // Signals
-    sigc::connection connect_drawing_event(sigc::slot<bool (GdkEvent*, Inkscape::DrawingItem *)> slot) {
+    sigc::connection connect_drawing_event(sigc::slot<bool(CanvasEvent const &, Inkscape::DrawingItem *)> slot) {
         return _drawing_event_signal.connect(slot);
     }
 
@@ -75,7 +75,7 @@ protected:
     bool _pick_outline = false;
 
     // Signals
-    sigc::signal<bool (GdkEvent*, Inkscape::DrawingItem *)> _drawing_event_signal;
+    sigc::signal<bool(CanvasEvent const &, Inkscape::DrawingItem *)> _drawing_event_signal;
 };
 
 } // namespace Inkscape

src/display/control/canvas-item.h
@@ -32,7 +32,6 @@
 #include <sigc++/sigc++.h>
 
 #include <gdkmm/cursor.h>
-#include <gdk/gdk.h>  // GdkEvent
 
 #include "canvas-item-enums.h"
 #include "canvas-item-buffer.h"
@@ -47,6 +46,7 @@ inline constexpr uint32_t CANVAS_ITEM_COLORS[] = { 0x0000ff7f, 0xff00007f, 0xfff
 
 namespace UI::Widget { class Canvas; }
 class CanvasItemGroup;
+class CanvasEvent;
 
 class CanvasItem
 {
@@ -98,10 +98,10 @@ public:
     // Events
     void set_pickable(bool pickable) { _pickable = pickable; }
     bool is_pickable() const { return _pickable; }
-    sigc::connection connect_event(sigc::slot<bool(GdkEvent*)> const &slot) {
+    sigc::connection connect_event(sigc::slot<bool(CanvasEvent const &)> const &slot) {
         return _event_signal.connect(slot);
     }
-    virtual bool handle_event(GdkEvent *event) {
+    virtual bool handle_event(CanvasEvent const &event) {
         return _event_signal.emit(event); // Default just emits event.
     }
 
@@ -141,7 +141,7 @@ protected:
     std::string _name; // For debugging
 
     // Events
-    sigc::signal<bool (GdkEvent*)> _event_signal;
+    sigc::signal<bool(CanvasEvent const &)> _event_signal;
 
     // Snapshotting
     template<typename F>

src/object/sp-guide.cpp
@@ -301,7 +301,7 @@ void SPGuide::showSPGuide(Inkscape::CanvasItemGroup *group)
 
     // Ensure event forwarding by the guide handle ("the dot") to the corresponding line
     auto dot = item->dot();
-    auto dot_handler = [=](GdkEvent *ev) { return sp_dt_guide_event(ev, item, this); };
+    auto dot_handler = [=](Inkscape::CanvasEvent const &ev) { return sp_dt_guide_event(ev, item, this); };
     dot->connect_event(dot_handler);
 
     views.emplace_back(item);

commit 89aba67a428f0fe32f7eae24f221da5340c3eed5

Fix TextTool when mouse off-canvas
Fix a regression from 20459d4d by reinstating some code that sends
events to the current tool when the mouse is off the canvas.

Fixes https://gitlab.com/inkscape/inkscape/-/issues/5189

src/ui/widget/canvas.cpp
@@ -41,6 +41,7 @@
 #include "color/cms-system.h"     // Color correction
 #include "color.h"          // Background color
 #include "desktop.h"
+#include "desktop-events.h"
 #include "display/control/canvas-item-drawing.h"
 #include "display/control/canvas-item-group.h"
 #include "display/control/snap-indicator.h"
@@ -1429,6 +1430,9 @@ bool CanvasPrivate::emit_event(CanvasEvent &event)
             if (item->handle_event(event)) return true;
             item = item->get_parent();
         }
+    } else if (q->_desktop && (event.type() == EventType::KEY_PRESS || event.type() == EventType::KEY_RELEASE)) {
+        // Pass keyboard events back to the desktop root handler so TextTool can work
+        return sp_desktop_root_handler(event, q->_desktop);
     }
 
     return false;