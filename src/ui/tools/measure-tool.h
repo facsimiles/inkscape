70b15227b5a8db9de0bb25ce577d206069580a63
Giambattista Caltabiano <giumbycaltabiano@gmail.com>
2025-01-10 13:08:01 +0000

    Adding delta measurements and shortcut to copy measurements to the clipboard to the Measure Tool
     * new delta measurements to measure tool (dX and dY)
     * adding an option to switch the delta measurements on/off
     * adding Alt-C shortcut to copy measurements to the clipboard

--- src/ui/tools/measure-tool.h
+++ src/ui/tools/measure-tool.h
@@ -22,11 +22,107 @@
 #include "display/control/canvas-item-ptr.h"
 #include <sigc++/scoped_connection.h>
 #include "ui/tools/tool-base.h"
+#include <glibmm/i18n.h>
 
 class SPKnot;
 namespace Inkscape { class CanvasItemCurve; }
 
 namespace Inkscape::UI::Tools {
+
+namespace MT {  // using namespace not to interfere with any other tool
+
+// indexes for the maps lengths labels symbols measureIsSet in std::vector<ClipboardMeaClass::Measure>
+// created outside the class otherwise we would have had to use te class name everytime
+enum LengthIDs
+{
+    SEGMENT,
+    LENGTH,
+    LENGTH_BETWEEN,
+    DX,
+    DY,
+    ANGLE,
+    SHAPE_LENGTH,
+    SHAPE_WIDTH,
+    SHAPE_HEIGHT,
+    SHAPE_X,
+    SHAPE_Y
+};
+
+/**
+ * This class stores the measurements that are copied to the clipboard.
+ * There are 3 maps for the lengths, labels and symbols.
+ * The maps are similar and can all be accessed with LengthIDs as indexes.
+ * The symbols (some of them currently) are used to label the measurements on the desktop (long labels would clutter the 
+ * screen even more...)
+ * 
+ */
+class ClipboardMeaClass
+{
+public:
+    ClipboardMeaClass();
+    ~ClipboardMeaClass();
+   
+    std::map<int,Glib::ustring> symbols {  // using to show on the workspace/canvas
+        {LengthIDs::SEGMENT, "S"},  
+        {LengthIDs::LENGTH, "L"},
+        {LengthIDs::LENGTH_BETWEEN, "lb"},
+        {LengthIDs::DX, "dX"},
+        {LengthIDs::DY, "dY"},
+        {LengthIDs::ANGLE, "α"},
+        {LengthIDs::SHAPE_LENGTH, "sl"},
+        {LengthIDs::SHAPE_WIDTH, "sw"},
+        {LengthIDs::SHAPE_HEIGHT, "sh"},
+        {LengthIDs::SHAPE_X, "sX"},
+        {LengthIDs::SHAPE_Y, "sY"},
+    };
+    std::map<int,Glib::ustring> labels {
+        {LengthIDs::SEGMENT, _("Segment")},
+        {LengthIDs::LENGTH, _("Length")},
+        {LengthIDs::LENGTH_BETWEEN, _("Length between")},
+        {LengthIDs::DX, _("dX")},
+        {LengthIDs::DY, _("dY")},
+        {LengthIDs::ANGLE, _("Angle")},
+        {LengthIDs::SHAPE_LENGTH, _("Shape length")},
+        {LengthIDs::SHAPE_WIDTH,  _("Shape width")},
+        {LengthIDs::SHAPE_HEIGHT, _("Shape height")},
+        {LengthIDs::SHAPE_X, _("Shape X")},
+        {LengthIDs::SHAPE_Y, _("Shape Y")},
+    };
+        std::map<int,bool> measureIsSet {  // if set to true the measurements are set and can be used
+        {LengthIDs::SEGMENT, false},
+        {LengthIDs::LENGTH, false},
+        {LengthIDs::LENGTH_BETWEEN, false},
+        {LengthIDs::DX, false},
+        {LengthIDs::DY, false},
+        {LengthIDs::ANGLE, false},
+        {LengthIDs::SHAPE_LENGTH, false},
+        {LengthIDs::SHAPE_WIDTH, false},
+        {LengthIDs::SHAPE_HEIGHT, false},
+        {LengthIDs::SHAPE_X, false},
+        {LengthIDs::SHAPE_Y, false},
+    };
+    std::map<int,double> lengths {
+        {LengthIDs::SEGMENT, 0}, // not used - just keeping maps the same...
+        {LengthIDs::LENGTH, 0},
+        {LengthIDs::LENGTH_BETWEEN, 0},
+        {LengthIDs::DX, 0},
+        {LengthIDs::DY, 0},
+        {LengthIDs::ANGLE, 0},
+        {LengthIDs::SHAPE_LENGTH, 0},
+        {LengthIDs::SHAPE_WIDTH, 0},
+        {LengthIDs::SHAPE_HEIGHT, 0},
+        {LengthIDs::SHAPE_X, 0},
+        {LengthIDs::SHAPE_Y, 0},
+    };
+    std::map<Glib::ustring,double> segLengths; // this is dynamic
+
+// composes the String without checking - need to check that the measurements are set (updated) before using it
+    Glib::ustring composeMeaStr(int id, int precision, Glib::ustring unit, bool withLabel = true, bool withUnit = true, bool tabSeparated = false);
+    void unsetShapeMeasures();
+};
+
+
+} // namespace MT
 
 class MeasureTool : public ToolBase
 {
@@ -56,7 +152,7 @@ public:
     void setMeasureCanvasText(bool is_angle, double precision, double amount, double fontsize,
                               Glib::ustring unit_name, Geom::Point position, guint32 background,
                               bool to_left, bool to_item, bool to_phantom,
-                              Inkscape::XML::Node *measure_repr);
+                              Inkscape::XML::Node *measure_repr, Glib::ustring label = "");
     void setMeasureCanvasItem(Geom::Point position, bool to_item, bool to_phantom,
                               Inkscape::XML::Node *measure_repr);
     void setMeasureCanvasControlLine(Geom::Point start, Geom::Point end, bool to_item, bool to_phantom,
@@ -73,6 +169,10 @@ public:
     void createAngleDisplayCurve(Geom::Point const &center, Geom::Point const &end, Geom::Point const &anchor,
                                  double angle, bool to_phantom,
                                  Inkscape::XML::Node *measure_repr = nullptr);
+    void copyToClipboard();
+    std::vector<Geom::Point> calcDeltaBasePoint(double dX, double dY);
+    bool equalWithinRange(double value, double reference_value, double epsilon, bool positiveAllowed = true, bool negativeAllowed = true);
+    MT::ClipboardMeaClass clipBMeas;
 
 private:
     std::optional<Geom::Point> explicit_base;
