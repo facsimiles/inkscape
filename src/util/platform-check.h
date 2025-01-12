bb88ddee606de6b455e43b415e5cd9a743920f1b
kaixoo <21697330-kaixoo@users.noreply.gitlab.com>
2025-01-12 11:02:41 +0200

    doc: add copyright headers

--- src/util/platform-check.h
+++ src/util/platform-check.h
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /**
+ * @file
  * Contains specific methods for platform-specific code: see if you're running GNOME, KDE...
+ */
+/* Authors:
+ *   Kaixoo
+ *
+ * Copyright (C) 2004-2024 Authors
+ *
+ * Released under GNU GPL v2+, read the file 'COPYING' for more information.
  */
 #ifndef INKSCAPE_UTIL_PLATFORM_CHECK_H
 #define INKSCAPE_UTIL_PLATFORM_CHECK_H
