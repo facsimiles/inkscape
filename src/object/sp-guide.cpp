facsimiles/inkscape

commit 0b4d3151ad419e0503b7d2a736a78aa9ad5940ff

Reactor Inkscape to use the new color model framework
Main movement of the previous color handling code:

 * Removal of color.cpp, which is now replaced by the class in colors/color
 * Removal of svg/svg-color.cpp, which is now replaced by colors/color for full color handling and colors/parser for parsing.
 * Removal of color-rgba.h which is now replaced by colors/color and ui/utils for Gdk conversions
 * Removal of color/color-conv.cpp into colors/parser
 * Removal of color/color-profile-cms-fns.h as all the cms encapsulation is already done above.
 * Removal of widgets/paintdef.cpp, which was a duplicate color structure, xml functionality moved to colors/xml-color.cpp
 * Removal of svg/svg-renderer.cpp color code, Gdk coversions moved to ui/utils and css code is now an Inkscape::Color object.
 * Removal of svg-color and oklab tests which are entirely replaced by colors/*tests
 * Refactor global-palettes.cpp to remove all custom rgb building and instead build Color objects directly.
 * Refactor color-icc-selector removing most of it's own code in favor of the cms and color space API
 * Moving of color/cmyk-conv.cpp into colors/spaces/cmyk and regularised into a color space class.
 * Moving of hsluv.cpp and oklab.cpp which are split and regularised into many colors/spaces/hsluv and ok color space classes
 * Moving of color/cms-* into colors/cms/* with some of the code from object/colorprofile.cpp to restrict the use of lcms2 to just this directory and make the accessing of cms profiles and transforms predictable and less error prone.
 * Moving of profile-manager.cpp functionality colors/tracker.cpp rewritten to create the new icc color space objects.
 * Moving of ui/selected-color.cpp functionality to colors/color-set rewritten to be a collection of trackable color objects.

Refactoring actions:

 * Refactor style color to remove SPColor and Href values, add a private Inkscape::Color and getter/setter functions.

src/object/sp-guide.cpp
@@ -30,9 +30,10 @@
 #include "sp-namedview.h"
 #include "sp-root.h"
 
+#include "colors/color.h"
+#include "colors/manager.h"
 #include "display/control/canvas-item-guideline.h"
 #include "object/sp-page.h"
-#include "svg/svg-color.h"
 #include "svg/svg.h"
 #include "util/numeric/converters.h"
 #include "util/units.h"
@@ -88,8 +89,10 @@ void SPGuide::release()
 void SPGuide::set(SPAttr key, const gchar *value) {
     switch (key) {
     case SPAttr::INKSCAPE_COLOR:
-        if (value) {
-            this->setColor(sp_svg_read_color(value, 0x0000ff00) | 0x7f);
+        if (auto c = Inkscape::Colors::Color::parse(value)) {
+            if (!c->hasOpacity())
+                c->addOpacity(0.5);
+            color = c->toRGBA();
         }
         break;
     case SPAttr::INKSCAPE_LABEL:

commit 18072a85a262f46f5deb209ca842ea75f603b06e

Update guide on undo guide colour change
Regression from 0b4d3151ad.

src/object/sp-guide.cpp
@@ -92,7 +92,7 @@ void SPGuide::set(SPAttr key, const gchar *value) {
         if (auto c = Inkscape::Colors::Color::parse(value)) {
             if (!c->hasOpacity())
                 c->addOpacity(0.5);
-            color = c->toRGBA();
+            setColor(c->toRGBA());
         }
         break;
     case SPAttr::INKSCAPE_LABEL: