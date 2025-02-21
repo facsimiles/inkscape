2ceb2e0e67f2fb80395a46b118afb60e2e2cfc17
VaibhavMalik4187 <vaibhavmalik2018@gmail.com>
2025-02-22 01:21:41 +0530

    Use Undo::maybeDone for swatch color slider
    This commit prevents undo history from being spammed by "Change color
    history" entries on updating the Swatch color using the color slider.
    
    Fixes: https://gitlab.com/inkscape/inkscape/-/issues/5486

--- src/ui/widget/swatch-selector.cpp
+++ src/ui/widget/swatch-selector.cpp
@@ -62,7 +62,7 @@ void SwatchSelector::_changedCb()
 
         if (auto stop = ngr->getFirstStop()) {
             stop->setColor(_colors->getAverage());
-            DocumentUndo::done(ngr->document, _("Change swatch color"), INKSCAPE_ICON("color-gradient"));
+            DocumentUndo::maybeDone(ngr->document, _("change-swatch-color"), _("Change swatch color"), INKSCAPE_ICON("color-gradient"));
         }
     }
 }
