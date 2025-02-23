775f7d51e80294f2ef5dc8776b47d018df4c5e4d
mike kowalski <michal_kowalski@hotmail.com>
2025-02-22 19:55:26 -0800

    Add UI settings section to the settings dialog

--- src/ui/dialog/settings-dialog.cpp
+++ src/ui/dialog/settings-dialog.cpp
@@ -16,6 +16,7 @@
 #include <gtkmm/accelkey.h>
 #include <gtkmm/box.h>
 #include <gtkmm/button.h>
+#include <gtkmm/dropdown.h>
 #include <gtkmm/entry.h>
 #include <gtkmm/enums.h>
 #include <gtkmm/label.h>
@@ -41,6 +42,7 @@
 #include "inkscape-application.h"
 #include "io/resource.h"
 #include "preferences.h"
+#include "settings-helpers.h"
 #include "ui/containerize.h"
 #include "ui/modifiers.h"
 #include "ui/popup-menu.h"
@@ -48,6 +50,7 @@
 #include "ui/util.h"
 #include "ui/widget/color-picker.h"
 #include "ui/widget/ink-spin-button.h"
+#include "ui/widget/preferences-widget.h"
 #include "util-string/ustring-format.h"
 #include "util/action-accel.h"
 #include "util/key-helpers.h"
@@ -146,8 +149,17 @@ constexpr int WHOLE  = 12 * ONE_COLUMN;
 constexpr int HALF   =  6 * ONE_COLUMN;
 constexpr int THIRD  =  4 * ONE_COLUMN;
 constexpr int QUARTER=  3 * ONE_COLUMN;
+constexpr int SIXTH  =  2 * ONE_COLUMN;
 
 const Glib::Quark NODE_KEY{"node-element"};
+
+XML::Node* get_widget_node(const Gtk::Widget* widget) {
+    if (!widget) return nullptr;
+
+    return static_cast<XML::Node*>(const_cast<Gtk::Widget*>(widget)->get_data(NODE_KEY));
+}
+
+const Glib::ustring settings_path("/dialogs/settings/");
 
 const char* get_modifier_key_name(GdkModifierType key) {
     static std::map<GdkModifierType, std::string, std::less<>> key_names = {
@@ -229,6 +241,13 @@ double to_number(std::string_view str, double default_val = 0.0) {
     return strtod(str.data(), nullptr);
 }
 
+std::string get_widget_id(const Gtk::Widget* widget) {
+    if (auto node = get_widget_node(widget)) {
+        return to_string(element_attr(node, "id"));
+    }
+    return {};
+}
+
 int to_size(std::string_view size, int default_size) {
     if (size.empty()) return default_size;
 
@@ -243,6 +262,9 @@ int to_size(std::string_view size, int default_size) {
     }
     else if (size == "quarter") {
         return QUARTER;
+    }
+    else if (size == "sixth") {
+        return SIXTH;
     }
     else {
         std::cerr << "Element size request " << size << " not recognized";
@@ -497,8 +519,8 @@ using Visibility = std::map<std::string, std::vector<Gtk::Widget*>>;
 
 // Widget construction context used while traversing XML UI file
 struct Context {
-    Context(XML::Document& ui, const Templates& templates, ReadWrite::IO* io, Observers& observers, Visibility& visibility)
-        : ui(ui), templates(templates), io(io), observers(observers), visibility(visibility)
+    Context(XML::Document& ui, const Templates& templates, ReadWrite::IO* io, Observers& observers, Visibility& visibility, int scaling_factor)
+        : ui(ui), templates(templates), io(io), observers(observers), visibility(visibility), scaling_factor(scaling_factor)
     {}
 
     XML::Document& ui;
@@ -507,6 +529,7 @@ struct Context {
     Glib::RefPtr<Gtk::SizeGroup> first_col;
     Observers& observers;
     Visibility& visibility;
+    int scaling_factor;
 };
 
 void add_visibility_observer(Context& ctx, Gtk::Widget* widget, XML::Node* node) {
@@ -524,7 +547,7 @@ void add_visibility_observer(Context& ctx, Gtk::Widget* widget, XML::Node* node)
         auto vis = &ctx.visibility;
         ctx.observers[path] = Preferences::PreferencesObserver::create(path, [path, vis](auto& value) {
             for (auto w : (*vis)[path]) {
-                auto element = static_cast<XML::Node*>(w->get_data(NODE_KEY));
+                auto element = get_widget_node(w);
                 if (!element) continue;
                 auto on = element_attr(element, "value");
                 w->set_visible(value.getString().raw() == on);
@@ -870,7 +893,16 @@ struct Panel : Gtk::Box {
         return _subgroup->get_visible();
     }
 
-    void set_expanded(bool expand) {
+    // std::string get_panel_unique_id() const {
+    //     if (auto node = get_widget_node(this)) {
+    //         auto section = element_attr(node->parent(), "id");
+    //         auto id = element_attr(node, "id");
+    //         return std::string(section) + "_" + std::string(id);
+    //     }
+    //     return {};
+    // }
+
+    void set_expanded(bool expand = true) {
         _subgroup->set_visible(expand);
         if (_header) _header->set_icon(expand ? "pan-down" : "pan-end");
         if (expand) {
@@ -878,6 +910,14 @@ struct Panel : Gtk::Box {
         }
         else {
             remove_css_class("open");
+        }
+        if (expand) {
+            // remember which panel is visible/expanded
+            if (auto node = get_widget_node(this)) {
+                // remember for each section separately
+                auto section = to_string(element_attr(node->parent(), "id"));
+                Preferences::get()->setString(settings_path + "showPanel/" + section, get_widget_id(this));
+            }
         }
     }
 
@@ -904,26 +944,49 @@ struct Panel : Gtk::Box {
 };
 
 struct Section : Gtk::ListBoxRow {
+    template <typename F>
+    Panel* for_each_panel(F f) {
+        auto& content = get_content(false);
+        for (auto widget : content.get_children()) {
+            if (auto panel = dynamic_cast<Panel*>(widget)) {
+                if (f(panel)) return panel;
+            }
+        }
+        return nullptr;
+    }
+
     Section(Context& ctx, XML::Node* node): _root(node) {
         _box.add_css_class("section");
         auto& content = get_content(false);
         build_ui(ctx, &content, node);
 
-        // int panel_index = 0;
-        for (auto widget : content.get_children()) {
-            if (auto panel = dynamic_cast<Panel*>(widget)) {
-                panel->set_expanded(false);
-                // panel_index++;
-                if (auto header = panel->get_header()) {
-                    header->button()->signal_clicked().connect([this, panel] {
-                        if (panel->is_expanded()) {
-                            // collapse other panels, show only this one
-                            expand_panel(panel);
-                        }
-                    });
-                }
+        for_each_panel([this](auto panel) {
+            panel->set_expanded(false);
+            if (auto header = panel->get_header()) {
+                header->button()->signal_clicked().connect([this, panel] {
+                    if (panel->is_expanded()) {
+                        // collapse other panels, show only this one
+                        expand_panel(panel);
+                    }
+                });
             }
-        }
+            return false; // not done
+        });
+
+        // for (auto widget : content.get_children()) {
+        //     if (auto panel = dynamic_cast<Panel*>(widget)) {
+        //         panel->set_expanded(false);
+        //         // panel_index++;
+        //         if (auto header = panel->get_header()) {
+        //             header->button()->signal_clicked().connect([this, panel] {
+        //                 if (panel->is_expanded()) {
+        //                     // collapse other panels, show only this one
+        //                     expand_panel(panel);
+        //                 }
+        //             });
+        //         }
+        //     }
+        // }
         auto title = to_label(node);
         auto label = Gtk::make_managed<Gtk::Label>(title);
         label->set_xalign(0);
@@ -931,15 +994,41 @@ struct Section : Gtk::ListBoxRow {
         set_child(*label);
     }
 
+    // expand given panel, collapse all others
     void expand_panel(Panel* expand_panel) {
-        auto& content = get_content(false);
-        for (auto widget : content.get_children()) {
-            if (auto panel = dynamic_cast<Panel*>(widget)) {
-                panel->set_expanded(panel == expand_panel);
-            }
-        }
+        for_each_panel([expand_panel](auto panel) {
+            panel->set_expanded(panel == expand_panel);
+            return false; // not finished
+        });
+        // auto& content = get_content(false);
+        // for (auto widget : content.get_children()) {
+        //     if (auto panel = dynamic_cast<Panel*>(widget)) {
+        //         panel->set_expanded(panel == expand_panel);
+        //     }
+        // }
     }
 
+    Panel* find_panel_by_id(const std::string& id) {
+        return for_each_panel([&id](auto panel) {
+            return get_widget_id(panel) == id;
+        });
+        // auto& content = get_content(false);
+        // for (auto widget : content.get_children()) {
+        //     if (auto panel = dynamic_cast<Panel*>(widget)) {
+        //         if (get_widget_id(panel) == id) {
+        //             return panel;
+        //         }
+        //     }
+        // }
+        // return nullptr;
+    }
+
+    Panel* get_first_panel() {
+        return for_each_panel([](auto panel) {
+            return true; // return first one found
+        });
+
+    }
     Gtk::Widget& get_content(bool create) {
         //TODO: create on first use?
         return _box;
@@ -977,7 +1066,7 @@ void build_ui(Context& ctx, Gtk::Widget* parent, XML::Node* node, std::function<
             if (!widget) {
                 // current node does not represent a widget; parse all other types here
                 int gap = parse_element(element);
-                if (gap > 0 && previous) {
+                if (gap > 0 && previous && !dynamic_cast<Header*>(previous)) {
                     // using margin here, because it is cheaper
                     previous->set_margin_bottom(gap);
                 }
@@ -1037,7 +1126,7 @@ Gtk::Widget* create_ui_element(Context& ctx, XML::Node* node) {
 
     if (name == "panel") {
         auto indent = element_attr(node, "indent");
-        auto switch_path = std::string(element_attr(node, "switch"));
+        // auto switch_path = std::string(element_attr(node, "switch"));
         auto panel = Gtk::make_managed<Panel>(indent.empty() || indent == "true");
         build_ui(ctx, panel->_subgroup, node, [panel](Gtk::Widget* widget) {
             if (auto header = dynamic_cast<Header*>(widget)) {
@@ -1087,6 +1176,12 @@ Gtk::Widget* create_ui_element(Context& ctx, XML::Node* node) {
             l->set_tooltip_text(tooltip);
             ctx.first_col->add_widget(*l);
             row->attach(*l, 0, 0);
+        }
+        if (element_attr(node, "reset-icon") == "yes") {
+            auto icon = create_icon("reset");
+            icon->set_tooltip_text(_("Requires restart to take effect"));
+            icon->add_css_class("reset-icon");
+            row->attach(*icon, 2, 0);
         }
         int new_row = 0;
         build_ui(ctx, row, node, [row, &new_row](Gtk::Widget* widget){ row->attach(*widget, 1, new_row++); });
@@ -1174,6 +1269,9 @@ Gtk::Widget* create_ui_element(Context& ctx, XML::Node* node) {
         }
         if (auto unit = element_attr(node, "unit"); unit.size()) {
             number->set_suffix(to_string(unit));
+        }
+        if (auto scale = element_attr(node, "scaling-factor"); scale.size()) {
+            number->set_scaling_factor(to_number(scale));
         }
         auto size = to_size(element_attr(node, "size"), HALF);
         number->set_size_request(size);
@@ -1285,6 +1383,46 @@ Gtk::Widget* create_ui_element(Context& ctx, XML::Node* node) {
         });
         return wnd;
     }
+    else if (name == "selector") {
+        auto search = element_attr(node, "search") == "yes";
+        auto selector = Settings::create_combobox(element_attr(node, "source"), ctx.scaling_factor, search);
+        if (!selector) {
+            throw std::runtime_error(std::string("Selector's source is not known: ") + std::string(element_attr(node, "source")));
+        }
+        selector->add_css_class("selector");
+        auto size = to_size(element_attr(node, "size"), WHOLE);
+        selector->set_size_request(size);
+        return selector;
+    }
+    else if (name == "font-selector") {
+        auto font_selector = Gtk::make_managed<Gtk::Entry>();
+        font_selector->add_css_class("font-selector");
+        auto pos = Gtk::Entry::IconPosition::SECONDARY;
+        font_selector->set_icon_from_icon_name("edit", pos);
+        font_selector->set_icon_activatable(true, pos);
+        font_selector->signal_icon_release().connect([node, io](auto icon){
+            auto dlg = std::make_unique<Gtk::FontChooserDialog>();
+            // if (_in_edit_mode) {
+            //     cancel_editing();
+            // }
+            // else {
+            //     edit_shortcut();
+            // }
+        });
+        font_selector->set_can_focus(false);
+        font_selector->set_focus_on_click(false);
+        font_selector->set_focusable(false);
+        auto size = to_size(element_attr(node, "size"), WHOLE);
+        font_selector->set_size_request(size);
+
+        return font_selector;
+    }
+    else if (name == "ruler") {
+        auto ruler = Gtk::make_managed<Widget::ZoomCorrRuler>();
+        ruler->set_hexpand();
+        ruler->add_css_class("ruler");
+        return ruler;
+    }
     else {
         // all other elements are not handled here
         return nullptr;
@@ -1299,6 +1437,7 @@ Section* create_section(Context& ctx, XML::Node* node) {
     label->set_margin_start(4);
     section->set_child(*label);
     section->set_visible();
+    section->set_data(NODE_KEY, node);
     return section;
 }
 
@@ -1338,9 +1477,6 @@ SettingsDialog::SettingsDialog(Gtk::Window& parent):
     _pages.set_vexpand();
     _pages.set_name("Pages");
     _hbox.append(_page_selector);
-    auto sep = Gtk::make_managed<Gtk::Separator>(Gtk::Orientation::VERTICAL);
-    sep->set_size_request(1);
-    _hbox.append(*sep);
     _hbox.append(_wnd);
     _wnd.set_expand();
     _wnd.set_has_frame(false);
@@ -1352,9 +1488,10 @@ SettingsDialog::SettingsDialog(Gtk::Window& parent):
     // access to preferences
     _io = std::make_unique<PreferencesIO>(get_root()->get_display());
 
+    int pages = 0;
     auto ui = _ui->root();
     try {
-        Context ctx(*_ui, _templates, _io.get(), *_observers, *_visibility);
+        Context ctx(*_ui, _templates, _io.get(), *_observers, *_visibility, get_scale_factor());
 
         for (auto node = ui->firstChild(); node; node = node->next()) {
             auto name = element_name(node);
@@ -1367,6 +1504,7 @@ SettingsDialog::SettingsDialog(Gtk::Window& parent):
                 ctx.first_col = Gtk::SizeGroup::create(Gtk::SizeGroup::Mode::HORIZONTAL);
                 auto section = create_section(ctx, node);
                 _pages.append(*section);
+                pages++;
                 _content.append(section->get_content(false));
             }
             else if (name == "comment") {
@@ -1379,7 +1517,10 @@ SettingsDialog::SettingsDialog(Gtk::Window& parent):
     }
     catch (std::exception& ex) {
         std::cerr << "Error creating settings dialog: " << ex.what() << std::endl;
+        return;
     }
+
+    auto selected_page = Preferences::get()->getString(settings_path + "selectedPage");
 
     _pages.signal_row_selected().connect([this](Gtk::ListBoxRow* row) {
         // show content of selected page
@@ -1388,10 +1529,39 @@ SettingsDialog::SettingsDialog(Gtk::Window& parent):
                 _content.remove(*c);
             }
             _content.append(section->get_content(true));
+
+            if (auto element = get_widget_node(row)) {
+                auto id = element_attr(element, "id");
+                Preferences::get()->setString(settings_path + "selectedPage", std::string(id));
+            }
+
+            auto expanded = Preferences::get()->getString(settings_path + "showPanel/" + get_widget_id(section));
+            auto panel = section->find_panel_by_id(expanded);
+            if (!panel) {
+                panel = section->get_first_panel();
+            }
+            if (panel) {
+                section->expand_panel(panel);
+            }
         }
     });
-    if (auto row = _pages.get_row_at_index(0)) {
-        _pages.select_row(*row);
+
+    for (int i = 0; i < pages; ++i) {
+        if (auto row = _pages.get_row_at_index(i)) {
+            auto element = get_widget_node(row);
+            if (!element) continue;
+            auto id = element_attr(element, "id");
+            if (id == selected_page.raw()) {
+                _pages.select_row(*row);
+                break;
+            }
+        }
+    }
+
+    if (!_pages.get_selected_row()) {
+        if (auto row = _pages.get_row_at_index(0)) {
+            _pages.select_row(*row);
+        }
     }
 
     get_content_area()->append(_hbox);
