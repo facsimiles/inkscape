ee57a69f7a302c4c5aacafe36fcc2022215924ca
PBS <pbs3141@gmail.com>
2025-04-21 20:46:49 +0200

    Avoid unchecked optional access in render_preview
    Fixes https://gitlab.com/inkscape/inkscape/-/issues/4744
    Fixes https://gitlab.com/inkscape/inkscape/-/issues/4786
    Fixes https://gitlab.com/inkscape/inkscape/-/issues/5387
    Fixes https://gitlab.com/inkscape/inkscape/-/issues/5518

--- src/util/preview.cpp
+++ src/util/preview.cpp
@@ -47,18 +47,21 @@ render_preview(SPDocument *doc, std::shared_ptr<Inkscape::Drawing> drawing, uint
     auto area = Geom::IntRect::from_xywh(ibox.min() - Geom::IntPoint(dx, dy), pdim);
 
     /* Actual renderable area */
-    Geom::IntRect ua = *Geom::intersect(ibox, area);
-    auto surface = Cairo::ImageSurface::create(Cairo::Surface::Format::ARGB32, ua.width(), ua.height());
+    auto const ua = Geom::intersect(ibox, area);
+    if (!ua) {
+        return {};
+    }
+    auto surface = Cairo::ImageSurface::create(Cairo::Surface::Format::ARGB32, ua->width(), ua->height());
 
     auto on_error = [&] (char const *err) {
         std::cerr << "render_preview: " << err << std::endl;
-        surface = Cairo::ImageSurface::create(Cairo::Surface::Format::ARGB32, ua.width(), ua.height());
+        surface = Cairo::ImageSurface::create(Cairo::Surface::Format::ARGB32, ua->width(), ua->height());
     };
 
     try {
         {
             auto cr = Cairo::Context::create(surface);
-            cr->rectangle(0, 0, ua.width(), ua.height());
+            cr->rectangle(0, 0, ua->width(), ua->height());
 
             // We always use checkerboard to indicate transparency.
             if (SP_RGBA32_A_F(bg) < 1.0) {
@@ -77,13 +80,13 @@ render_preview(SPDocument *doc, std::shared_ptr<Inkscape::Drawing> drawing, uint
         drawing->root()->setTransform(Geom::Scale(sf));
         drawing->update();
 
-        auto dc = Inkscape::DrawingContext(surface->cobj(), ua.min());
+        auto dc = Inkscape::DrawingContext(surface->cobj(), ua->min());
         if (item) {
             // Render just one item
-            item->render(dc, ua);
+            item->render(dc, *ua);
         } else {
             // Render drawing.
-            drawing->render(dc, ua);
+            drawing->render(dc, *ua);
         }
 
         surface->flush();
