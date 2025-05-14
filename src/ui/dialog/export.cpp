790142842f5824886c2621085d0cabc4e2436a2b
Vitalii Shmorhun <3dvits@gmail.com>
2025-05-14 17:23:43 +0300

    If the name contains a relative path, use it as the export path
    The inkscape:export-filename attribute can contain not just the name of
    the file, but the relative path. In this case, the function appends this
    value to the existing path, each time extending the export path with
    additional directories.
    
    Fixes https://gitlab.com/inkscape/inkscape/-/issues/5429

--- src/ui/dialog/export.cpp
+++ src/ui/dialog/export.cpp
@@ -459,6 +459,10 @@ std::string Export::prependDirectory(Glib::ustring name, const std::string &orig
     if (Glib::path_is_absolute(name) || name.empty())
         return name;
 
+    if (Glib::path_get_basename(name) != name) {
+        return name;
+    }
+
     std::string directory;
 
     if (!orig.empty()) {
