e2c80d076a3df6c81750e4bcccae540b98bcfc87
David Burghoff <burghoff@gmail.com>
2025-09-06 23:08:19 +0000

    Add additional typelibs for Windows installations

--- CMakeScripts/InstallMSYS2.cmake
+++ CMakeScripts/InstallMSYS2.cmake
@@ -278,6 +278,9 @@ if(WIN32)
     ${MINGW_LIB}/girepository-1.0/PangoFc-1.0.typelib
     ${MINGW_LIB}/girepository-1.0/PangoFT2-1.0.typelib
     ${MINGW_LIB}/girepository-1.0/freetype2-2.0.typelib
+    ${MINGW_LIB}/girepository-1.0/Gdk-4.0.typelib
+    ${MINGW_LIB}/girepository-1.0/Gsk-4.0.typelib
+    ${MINGW_LIB}/girepository-1.0/Graphene-1.0.typelib
     DESTINATION lib/girepository-1.0)
 
   # Aspell dictionaries
