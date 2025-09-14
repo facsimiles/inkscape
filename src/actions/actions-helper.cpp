facsimiles/inkscape

commit 68707a184b5b06192672e267c80114529848c4a6

Refactor toolbar lifecycle
Refactor the toolbars and their lifecycle model in preparation for a
multi-tabbed UI. The main changes are

* Toolbars are now lazy-loaded.

* Toolbars are activated by calling setDesktop(). Only one toolbar is
  active at a time. Toolbars are always deactivated before destruction.

* For toolbars with units, the unit tracking is now done through a
  setActiveUnit() function, rather than traversing the widget tree and
  checking names / dynamic casting.

* Toolbar expand/collapse logic is split off into a separate base class
  ToolbarWidget, while Toolbar is reserved just for tool toolbars.

* Defocus widget handling now uses the DefocusTarget interface.

The minor changes are:

* Don't store GtkBuilder forever, since only needed during construction.
* Conventionalise the namespacing and include guards of toolbar files.
* Make ToolbarWidget's constructor responsible for adding the child box,
  ensuring it is always set.
* Use OperationBlocker instead of boolean _freeze variables.
* Batch updates together in object toolbars (e.g. rectangle, arc).

Fixes:

* Booleans toolbar:
  - Fix both '+' and '-' buttons being initially active.
* CalligraphicProfileRename dialog:
  - Fix crash from inappropriate property_destroy_with_parent = true.
* Pencil toolbar:
  - Merge copypasta'd simplify_flatten() and flatten_spiro_bspline().
* Fix SpinButton's GTK4-broken Esc/Enter/Tab/Ctrl+Z behaviour, which no
  longer needs the _stay variable.
* Make opacity spinbutton in statusbar defocus on Esc.

src/ui/toolbar/toolbars.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /** @file
  *
- * A container for tool toolbars, displaying one toolbar at a time.
+ * A container for toolbars, displaying one toolbar at a time.
  *
  *//*
  * Authors:
@@ -9,19 +9,14 @@
  *  Alex Valavanis
  *  Mike Kowalski
  *  Vaibhav Malik
+ *  PBS
  *
- * Copyright (C) 2023 Tavmjong Bah
+ * Copyright (C) 2024 PBS
  *
  * Released under GNU GPL v2+, read the file 'COPYING' for more information.
  */
 
 #include "toolbars.h"
-
-#include <iostream>
-#include <glibmm/i18n.h>
-#include <gtkmm/button.h>
-#include <gtkmm/grid.h>
-#include <gtkmm/spinbutton.h>
 
 // For creating toolbars
 #include "ui/toolbar/arc-toolbar.h"
@@ -50,108 +45,107 @@
 #include "ui/toolbar/tweak-toolbar.h"
 #include "ui/toolbar/zoom-toolbar.h"
 #include "ui/tools/tool-base.h"
-#include "ui/util.h"
-#include "ui/widget/style-swatch.h"
 
 namespace Inkscape::UI::Toolbar {
 namespace {
 
 // Data for building and tracking toolbars.
-struct ToolBoxData
+struct ToolbarData
 {
-    char const *type_name; // Used by preferences
-    Glib::ustring const tool_name;
-    std::unique_ptr<Toolbar> (*create)(SPDesktop *desktop);
+    Glib::ustring name;
+    std::unique_ptr<Toolbar> (*create)();
 };
 
 template <typename T, auto... args>
-auto create = [](SPDesktop *desktop) -> std::unique_ptr<Toolbar> { return std::make_unique<T>(desktop, args...); };
+auto create = [] () -> std::unique_ptr<Toolbar> { return std::make_unique<T>(args...); };
 
-ToolBoxData const aux_toolboxes[] = {
-    // If you change the tool_name for Measure or Text here, change it also in desktop-widget.cpp.
+auto const toolbar_data = std::unordered_map<std::string, ToolbarData>{
     // clang-format off
-    { "/tools/select",          "Select",       create<SelectToolbar>},
-    { "/tools/nodes",           "Node",         create<NodeToolbar>},
-    { "/tools/booleans",        "Booleans",     create<BooleansToolbar>},
-    { "/tools/marker",          "Marker",       create<MarkerToolbar>},
-    { "/tools/shapes/rect",     "Rect",         create<RectToolbar>},
-    { "/tools/shapes/arc",      "Arc",          create<ArcToolbar>},
-    { "/tools/shapes/star",     "Star",         create<StarToolbar>},
-    { "/tools/shapes/3dbox",    "3DBox",        create<Box3DToolbar>},
-    { "/tools/shapes/spiral",   "Spiral",       create<SpiralToolbar>},
-    { "/tools/freehand/pencil", "Pencil",       create<PencilToolbar, true>},
-    { "/tools/freehand/pen",    "Pen",          create<PencilToolbar, false>},
-    { "/tools/calligraphic",    "Calligraphic", create<CalligraphyToolbar>},
-    { "/tools/text",            "Text",         create<TextToolbar>},
-    { "/tools/gradient",        "Gradient",     create<GradientToolbar>},
-    { "/tools/mesh",            "Mesh",         create<MeshToolbar>},
-    { "/tools/zoom",            "Zoom",         create<ZoomToolbar>},
-    { "/tools/measure",         "Measure",      create<MeasureToolbar>},
-    { "/tools/dropper",         "Dropper",      create<DropperToolbar>},
-    { "/tools/tweak",           "Tweak",        create<TweakToolbar>},
-    { "/tools/spray",           "Spray",        create<SprayToolbar>},
-    { "/tools/connector",       "Connector",    create<ConnectorToolbar>},
-    { "/tools/pages",           "Pages",        create<PageToolbar>},
-    { "/tools/paintbucket",     "Paintbucket",  create<PaintbucketToolbar>},
-    { "/tools/eraser",          "Eraser",       create<EraserToolbar>},
-    { "/tools/lpetool",         "LPETool",      create<LPEToolbar>},
-    { "/tools/picker",          "ObjectPicker", create<ObjectPickerToolbar>},
-    { nullptr,                  "",             nullptr}
+    {"/tools/select",          {"Select",       create<SelectToolbar>}},
+    {"/tools/nodes",           {"Node",         create<NodeToolbar>}},
+    {"/tools/booleans",        {"Booleans",     create<BooleansToolbar>}},
+    {"/tools/marker",          {"Marker",       create<MarkerToolbar>}},
+    {"/tools/shapes/rect",     {"Rect",         create<RectToolbar>}},
+    {"/tools/shapes/arc",      {"Arc",          create<ArcToolbar>}},
+    {"/tools/shapes/star",     {"Star",         create<StarToolbar>}},
+    {"/tools/shapes/3dbox",    {"3DBox",        create<Box3DToolbar>}},
+    {"/tools/shapes/spiral",   {"Spiral",       create<SpiralToolbar>}},
+    {"/tools/freehand/pencil", {"Pencil",       create<PencilToolbar, true>}},
+    {"/tools/freehand/pen",    {"Pen",          create<PencilToolbar, false>}},
+    {"/tools/calligraphic",    {"Calligraphic", create<CalligraphyToolbar>}},
+    {"/tools/text",            {"Text",         create<TextToolbar>}},
+    {"/tools/gradient",        {"Gradient",     create<GradientToolbar>}},
+    {"/tools/mesh",            {"Mesh",         create<MeshToolbar>}},
+    {"/tools/zoom",            {"Zoom",         create<ZoomToolbar>}},
+    {"/tools/measure",         {"Measure",      create<MeasureToolbar>}},
+    {"/tools/dropper",         {"Dropper",      create<DropperToolbar>}},
+    {"/tools/tweak",           {"Tweak",        create<TweakToolbar>}},
+    {"/tools/spray",           {"Spray",        create<SprayToolbar>}},
+    {"/tools/connector",       {"Connector",    create<ConnectorToolbar>}},
+    {"/tools/pages",           {"Pages",        create<PageToolbar>}},
+    {"/tools/paintbucket",     {"Paintbucket",  create<PaintbucketToolbar>}},
+    {"/tools/eraser",          {"Eraser",       create<EraserToolbar>}},
+    {"/tools/lpetool",         {"LPETool",      create<LPEToolbar>}},
+    {"/tools/picker",          {"ObjectPicker", create<ObjectPickerToolbar>}},
     // clang-format on
 };
 
 } // namespace
 
-// We only create an empty box, it is filled later after the desktop is created.
 Toolbars::Toolbars()
-    : Gtk::Box(Gtk::Orientation::VERTICAL)
+    : Gtk::Box{Gtk::Orientation::VERTICAL}
 {
-    set_name("Tool-Toolbars");
+    set_name("Toolbars");
 }
 
-// Fill the toolbars widget with toolbars.
-// Toolbars are contained inside a grid with an optional swatch.
-void Toolbars::create_toolbars(SPDesktop *desktop)
+Toolbars::~Toolbars()
 {
-    // Create the toolbars using their "create" methods.
-    for (int i = 0; aux_toolboxes[i].type_name; i++) {
-        if (aux_toolboxes[i].create) {
-            // Change create_func to return Gtk::Box!
-            auto const sub_toolbox = Gtk::manage(aux_toolboxes[i].create(desktop).release());
-            sub_toolbox->set_name("SubToolBox");
-            sub_toolbox->set_hexpand();
+    if (_current_toolbar) {
+        _current_toolbar->setDesktop(nullptr);
+    }
+}
 
-            // Use a grid to wrap the toolbar and a possible swatch.
-            auto const grid = Gtk::make_managed<Gtk::Grid>();
+void Toolbars::setTool(Tools::ToolBase *tool)
+{
+    // Acquire the toolbar to be shown, possibly null
+    Toolbar *toolbar = nullptr;
 
-            // Store a pointer to the grid so we can show/hide it as the tool changes.
-            toolbar_map[aux_toolboxes[i].tool_name] = grid;
+    if (tool) {
+        auto &toolbars_entry = _toolbars[tool->getPrefsPath()];
 
-            Glib::ustring ui_name = aux_toolboxes[i].tool_name +
-                                    "Toolbar"; // If you change "Toolbar" here, change it also in desktop-widget.cpp.
-            grid->set_name(ui_name);
-
-            grid->attach(*sub_toolbox, 0, 0, 1, 1);
-
-            append(*grid);
+        if (!toolbars_entry) {
+            // Lazily create the toolbar.
+            auto const &data = toolbar_data.at(tool->getPrefsPath());
+            toolbars_entry = data.create();
+            toolbars_entry->set_name(data.name + "Toolbar");
+            toolbars_entry->set_hexpand();
+            append(*toolbars_entry);
         }
+
+        toolbar = toolbars_entry.get();
     }
 
-    desktop->connectEventContextChanged(sigc::mem_fun(*this, &Toolbars::change_toolbar));
-
-    // Show initial toolbar, hide others.
-    change_toolbar(desktop, desktop->getTool());
+    if (toolbar != _current_toolbar) {
+        // Tool has changed.
+        if (_current_toolbar) {
+            _current_toolbar->set_visible(false);
+            _current_toolbar->setDesktop(nullptr);
+        }
+        _current_toolbar = toolbar;
+        if (_current_toolbar) {
+            _current_toolbar->setDesktop(tool->getDesktop());
+            _current_toolbar->set_visible(true);
+        }
+    } else if (_current_toolbar && tool->getDesktop() != _current_toolbar->getDesktop()) {
+        // Tool has stayed the same but desktop has changed.
+        _current_toolbar->setDesktop(tool->getDesktop());
+    }
 }
 
-void Toolbars::change_toolbar(SPDesktop * /*desktop*/, Tools::ToolBase *tool)
+void Toolbars::setActiveUnit(Util::Unit const *unit)
 {
-    if (!tool) {
-        std::cerr << "Toolbars::change_toolbar: tool is null!" << std::endl;
-        return;
-    }
-
-    for (int i = 0; aux_toolboxes[i].type_name; i++) {
-        toolbar_map[aux_toolboxes[i].tool_name]->set_visible(tool->getPrefsPath() == aux_toolboxes[i].type_name);
+    if (_current_toolbar) {
+        _current_toolbar->setActiveUnit(unit);
     }
 }
 

commit a0b731976117aeb26cd56234fe2da5adbe82e161

Fix toolbar button shortcut handling
* Handle "d" (double) variant (used by rotate actions).

  This fixes the startup warning
  ```
  Shortcuts::update_gui_text_recursive: unhandled variant type: d
  ```

* Add missing tooltips for rotate actions with 90 or -90 degrees.

* Fix missing shortcuts in tooltips for all toolbars other than the
  select tool. Regression from 68707a184b due to lazy loading.

* Remove unnecessary tooltips from .ui files; they are set in code.

share/ui/toolbar-select.ui
@@ -112,7 +112,6 @@
         <property name="valign">center</property>
         <property name="focusable">True</property>
         <property name="focus-on-click">False</property>
-        <property name="tooltip-text" translatable="yes">Rotate selection 90\xc2\xb0 counter-clockwise</property>
         <property name="action-name">app.transform-rotate</property>
         <property name="action-target">-90.0</property>
         <property name="has-frame">False</property>
@@ -134,7 +133,6 @@
         <property name="valign">center</property>
         <property name="focusable">True</property>
         <property name="focus-on-click">False</property>
-        <property name="tooltip-text" translatable="yes">Rotate selection 90\xc2\xb0 clockwise</property>
         <property name="action-name">app.transform-rotate</property>
         <property name="action-target">90.0</property>
         <property name="has-frame">False</property>
@@ -156,7 +154,6 @@
         <property name="valign">center</property>
         <property name="focusable">True</property>
         <property name="focus-on-click">False</property>
-        <property name="tooltip-text" translatable="yes">Flip selected objects horizontally</property>
         <property name="action-name">app.object-flip-horizontal</property>
         <property name="has-frame">False</property>
         <style>
@@ -177,7 +174,6 @@
         <property name="valign">center</property>
         <property name="focusable">True</property>
         <property name="focus-on-click">False</property>
-        <property name="tooltip-text" translatable="yes">Flip selected objects vertically</property>
         <property name="action-name">app.object-flip-vertical</property>
         <property name="has-frame">False</property>
         <style>

src/actions/actions-helper.cpp
@@ -15,6 +15,7 @@
 #include <glibmm/convert.h>
 #include <glibmm/miscutils.h>
 #include <glibmm/ustring.h>
+#include <double-conversion/double-conversion.h>
 
 #include "inkscape-application.h"
 #include "xml/document.h"          // for Document
@@ -98,6 +99,29 @@ get_document_and_selection(InkscapeApplication* app, SPDocument** document, Inks
 
     return true;
 }
+
+/**
+ * Convert a double to a string in a way that is compatible with GTK action parsing.
+ *
+ * This means locale-independent conversion to the shortest possible string that still
+ * has a ".0" at the end (so GTK doesn't confuse it with an integer).
+ */
+std::string to_string_for_actions(double x)
+{
+    using namespace double_conversion;
+    auto const conv = DoubleToStringConverter{
+        DoubleToStringConverter::UNIQUE_ZERO |
+            DoubleToStringConverter::EMIT_TRAILING_DECIMAL_POINT |
+            DoubleToStringConverter::EMIT_TRAILING_ZERO_AFTER_POINT,
+        "inf", "NaN", 'e', -3, 6, 0, 0
+    };
+    auto buf = std::string(32, '\0');
+    auto builder = double_conversion::StringBuilder(buf.data(), buf.size());
+    conv.ToShortest(x, &builder);
+    buf.resize(builder.position());
+    return buf;
+}
+
 
 /*
   Local Variables:

src/actions/actions-helper.h
@@ -33,6 +33,7 @@ void active_window_end_helper();
 std::string get_active_desktop_commands_location();
 void show_output(Glib::ustring const &data, bool is_cerr = true);
 bool get_document_and_selection(InkscapeApplication* app, SPDocument** document, Inkscape::Selection** selection);
+std::string to_string_for_actions(double x);
 
 #endif // INK_ACTIONS_HELPER_H
 

src/actions/actions-object.cpp
@@ -477,7 +477,7 @@ add_actions_object(InkscapeApplication* app)
     gapp->add_action(                "object-set-inverse-mask",         sigc::bind(sigc::ptr_fun(&object_mask_set_inverse),       app));
     gapp->add_action(                "object-release-mask",             sigc::bind(sigc::ptr_fun(&object_mask_release),           app));
 
-    // Depricated, see app.transform-rotate(90)
+    // Deprecated, see app.transform-rotate(90)
     gapp->add_action(                "object-rotate-90-cw",             sigc::bind(sigc::ptr_fun(&object_rotate_90_cw),           app));
     gapp->add_action(                "object-rotate-90-ccw",            sigc::bind(sigc::ptr_fun(&object_rotate_90_ccw),          app));
     gapp->add_action(                "object-flip-horizontal",          sigc::bind(sigc::ptr_fun(&object_flip_horizontal),        app));

src/actions/actions-transform.cpp
@@ -148,6 +148,8 @@ std::vector<std::vector<Glib::ustring>> raw_data_transform = {
     {"app.transform-rotate",        N_("Rotate"),             SECTION, N_("Rotate selected objects")},
     {"app.transform-rotate-step",   N_("Rotate Step"),        SECTION, N_("Rotate selected objects by multiple of step value")},
     {"app.transform-rotate-screen", N_("Rotate Screen"),      SECTION, N_("Rotate selected objects relative to zoom level")},
+    {"app.transform-rotate(90.0)",  N_("Object Rotate 90°"),   SECTION, N_("Rotate selected objects 90° clockwise")},
+    {"app.transform-rotate(-90.0)", N_("Object Rotate 90° CCW"), SECTION, N_("Rotate selected objects 90° counter-clockwise")},
 
     {"app.transform-remove",        N_("Remove Transforms"),  SECTION, N_("Remove any transforms from selected objects")},
     {"app.transform-reapply",       N_("Reapply Transforms"), SECTION, N_("Reapply the last transformation to the selection")},

src/ui/shortcuts.cpp
@@ -20,6 +20,7 @@
 #include <gtkmm/eventcontrollerkey.h>
 #include <gtkmm/shortcut.h>
 
+#include "actions/actions-helper.h"
 #include "document.h"
 #include "inkscape-application.h"
 #include "inkscape-window.h"
@@ -245,6 +246,9 @@ Shortcuts::update_gui_text_recursive(Gtk::Widget* widget)
                     action += "('" + variant + "')";
                 } else if (type == "i") {
                     variant = std::to_string(static_cast<Glib::Variant<std::int32_t> const &>(value).get());
+                    action += "(" + variant + ")";
+                } else if (type == "d") {
+                    variant = to_string_for_actions(static_cast<Glib::Variant<double> const &>(value).get());
                     action += "(" + variant + ")";
                 } else {
                     std::cerr << "Shortcuts::update_gui_text_recursive: unhandled variant type: " << type << std::endl;

src/ui/toolbar/toolbars.cpp
@@ -18,6 +18,8 @@
 
 #include "toolbars.h"
 
+#include "ui/shortcuts.h"
+
 // For creating toolbars
 #include "ui/toolbar/arc-toolbar.h"
 #include "ui/toolbar/booleans-toolbar.h"
@@ -119,6 +121,7 @@ void Toolbars::setTool(Tools::ToolBase *tool)
             toolbars_entry = data.create();
             toolbars_entry->set_name(data.name + "Toolbar");
             toolbars_entry->set_hexpand();
+            Shortcuts::getInstance().update_gui_text_recursive(toolbars_entry.get());
             append(*toolbars_entry);
         }
 

testfiles/src/actions-svg-processing.cpp
@@ -12,10 +12,11 @@
 
 #include <gtest/gtest.h>
 
-#include <src/document.h>
-#include <src/inkscape.h>
-#include <src/object/sp-root.h>
-#include <src/object/sp-object.h>
+#include "src/actions/actions-helper.h"
+#include "src/document.h"
+#include "src/inkscape.h"
+#include "src/object/sp-root.h"
+#include "src/object/sp-object.h"
 
 #include "geom-predicates.h"
 
@@ -103,4 +104,10 @@ TEST_F(ObjectLinksTest, removeTransforms)
         ASSERT_TRUE(new_box) << " item bounds '" << id << "'";
         EXPECT_RECT_NEAR(old_box, *new_box, 0.01) << "id";
     }
+}
+
+TEST(ActionsTest, to_string_for_actions)
+{
+    ASSERT_EQ(to_string_for_actions(90.0), "90.0");
+    ASSERT_EQ(to_string_for_actions(-90.0), "-90.0");
 }