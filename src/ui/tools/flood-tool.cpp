facsimiles/inkscape

commit 0b4d3151ad419e0503b7d2a736a78aa9ad5940ff

Reactor Inkscape to use the new color model framework
Main movement of the previous color handling code:

 * Removal of color.cpp, which is now replaced by the class in colors/color
 * Removal of svg/svg-color.cpp, which is now replaced by colors/color for full color handling and colors/parser for parsing.
 * Removal of color-rgba.h which is now replaced by colors/color and ui/utils for Gdk conversions
 * Removal of color/color-conv.cpp into colors/parser
 * Removal of color/color-profile-cms-fns.h as all the cms encapsulation is already done above.
 * Removal of widgets/paintdef.cpp, which was a duplicate color structure, xml functionality moved to colors/xml-color.cpp
 * Removal of svg/svg-renderer.cpp color code, Gdk coversions moved to ui/utils and css code is now an Inkscape::Color object.
 * Removal of svg-color and oklab tests which are entirely replaced by colors/*tests
 * Refactor global-palettes.cpp to remove all custom rgb building and instead build Color objects directly.
 * Refactor color-icc-selector removing most of it's own code in favor of the cms and color space API
 * Moving of color/cmyk-conv.cpp into colors/spaces/cmyk and regularised into a color space class.
 * Moving of hsluv.cpp and oklab.cpp which are split and regularised into many colors/spaces/hsluv and ok color space classes
 * Moving of color/cms-* into colors/cms/* with some of the code from object/colorprofile.cpp to restrict the use of lcms2 to just this directory and make the accessing of cms profiles and transforms predictable and less error prone.
 * Moving of profile-manager.cpp functionality colors/tracker.cpp rewritten to create the new icc color space objects.
 * Moving of ui/selected-color.cpp functionality to colors/color-set rewritten to be a collection of trackable color objects.

Refactoring actions:

 * Refactor style color to remove SPColor and Href values, add a private Inkscape::Color and getter/setter functions.

src/ui/tools/flood-tool.cpp
@@ -30,7 +30,6 @@
 #include <2geom/pathvector.h>
 
 #include "async/progress.h"
-#include "color.h"
 #include "context-fns.h"
 #include "desktop-style.h"
 #include "desktop.h"
@@ -69,6 +68,8 @@ using Inkscape::DocumentUndo;
 using Inkscape::Display::ExtractARGB32;
 using Inkscape::Display::ExtractRGB32;
 using Inkscape::Display::AssembleARGB32;
+
+using namespace Inkscape::Colors;
 
 namespace Inkscape::UI::Tools {
 
@@ -192,8 +193,6 @@ static bool compare_uint32(uint32_t a, uint32_t b, uint32_t d)
  */
 static bool compare_pixels(uint32_t check, uint32_t orig, uint32_t merged_orig_pixel, uint32_t dtc, int threshold, PaintBucketChannels method)
 {
-    float hsl_check[3] = {0,0,0}, hsl_orig[3] = {0,0,0};
-
     uint32_t ac = 0, rc = 0, gc = 0, bc = 0;
     ExtractARGB32(check, ac, rc, gc, bc);
 
@@ -206,13 +205,16 @@ static bool compare_pixels(uint32_t check, uint32_t orig, uint32_t merged_orig_p
     uint32_t amop = 0, rmop = 0, gmop = 0, bmop = 0;
     ExtractARGB32(merged_orig_pixel, amop, rmop, gmop, bmop);
 
+    auto hsl_orig = Color(Space::Type::HSL, {0, 0, 0});
+    auto hsl_check = hsl_orig;
+
     if ((method == FLOOD_CHANNELS_H) ||
         (method == FLOOD_CHANNELS_S) ||
         (method == FLOOD_CHANNELS_L)) {
-        double dac = ac;
         double dao = ao;
-        SPColor::rgb_to_hsl_floatv(hsl_check, rc / dac, gc / dac, bc / dac);
-        SPColor::rgb_to_hsl_floatv(hsl_orig, ro / dao, go / dao, bo / dao);
+        double dac = ac;
+        hsl_orig.set(Color(Space::Type::RGB, {ro / dao, go / dao, bo / dao}), true);
+        hsl_check.set(Color(Space::Type::RGB, {rc / dac, gc / dac, bc / dac}), true);
     }
     
     switch (method) {
@@ -720,7 +722,7 @@ static void sp_flood_do_flood_fill(SPDesktop *desktop, Geom::Point const &cursor
     auto const stride = Cairo::ImageSurface::format_stride_for_width(Cairo::Surface::Format::ARGB32, width);
     // TODO: C++20: *once* Apple+AppImage support it: Use std::make_unique_for_overwrite()
     auto const px = std::make_unique<unsigned char[]>(stride * height);
-    uint32_t bgcolor, dtc;
+    uint32_t dtc;
 
     // Draw image into data block px
     { // this block limits the lifetime of Drawing and DrawingContext
@@ -738,12 +740,11 @@ static void sp_flood_do_flood_fill(SPDesktop *desktop, Geom::Point const &cursor
         auto dc = DrawingContext(surf->cobj(), Geom::Point());
         // cairo_translate not necessary here - surface origin is at 0,0
 
-        bgcolor = document->getPageManager().background_color;
-        bgcolor &= 0xffffff00; // make color transparent for 'alpha' flood mode to work
-        // bgcolor is 0xrrggbbaa, we need 0xaarrggbb
-        dtc = bgcolor >> 8; // keep color transparent; page color doesn't support transparency anymore
+        // make color transparent for 'alpha' flood mode to work
+        auto bgcolor = document->getPageManager().getBackgroundColor();
+        bgcolor.setOpacity(0.0);
 
-        dc.setSource(bgcolor);
+        dc.setSource(bgcolor.toARGB());
         dc.setOperator(CAIRO_OPERATOR_SOURCE);
         dc.paint();
         dc.setOperator(CAIRO_OPERATOR_OVER);

commit 606dceb3a939dde18cbf3cb24514f0ba9e78afa8

Unregress paint bucket tool
Initialise the variable dtc, which was being garbage-read.
Regression from 0b4d3151ad41.

Fixes https://gitlab.com/inkscape/inkscape/-/issues/5933

src/ui/tools/flood-tool.cpp
@@ -732,6 +732,7 @@ static void sp_flood_do_flood_fill(SPDesktop *desktop, Geom::Point const &cursor
         // make color transparent for 'alpha' flood mode to work
         auto bgcolor = document->getPageManager().getBackgroundColor();
         bgcolor.setOpacity(0.0);
+        dtc = bgcolor.toARGB();
 
         dc.setSource(bgcolor);
         dc.setOperator(CAIRO_OPERATOR_SOURCE);