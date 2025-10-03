494869dedc9519468a46d328fa561655d00c3d9e
avee12x2 <aaravoswal@gmail.com>
2025-10-04 00:04:24 +0530

    Text-on-Path Squiggle Rendering compatibility added in canvas-item-squiggle

--- src/display/control/canvas-item-squiggle.h
+++ src/display/control/canvas-item-squiggle.h
@@ -19,9 +19,12 @@ class CanvasItemSquiggle : public CanvasItem
 {
 public:
     CanvasItemSquiggle(CanvasItemGroup *group, Geom::Point const &start, Geom::Point const &end, uint32_t color = 0xff0000ff);
+    CanvasItemSquiggle(CanvasItemGroup *group, std::vector<Geom::Point> const &points, uint32_t color = 0xff0000ff);
 
     // Properties
     void set_points(Geom::Point start, Geom::Point end);
+    void set_points(std::vector<Geom::Point> const &points);
+    void set_squiggle_params(double amplitude = 3.5, double wavelength = 8.0, double sample_dt = 0.02);
     void set_color(uint32_t color);
 
 protected:
@@ -35,7 +38,12 @@ private:
     Geom::Point _start;
     Geom::Point _end;
 
+    std::vector<Geom::Point> _points;
+
     uint32_t _color;
+    double _amplitude;   // Amplitude of the squiggle in canvas units
+    double _wavelength;  // Wavelength of the squiggle in canvas units
+    double _sample_dt;   // Sampling step for drawing the squiggle
 
     Geom::PathVector _squiggle_path;
 
