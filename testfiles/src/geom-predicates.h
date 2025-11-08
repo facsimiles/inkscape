6f63149a5d891e9de2ab8426f0da13c3ca7003f7
PBS <pbs3141@gmail.com>
2025-11-08 16:28:14 +0100

    Prevent use of "point.h"-style include with internal 2geom
    Force such includes to be written <2geom/point.h>, and ensure IDEs
    insert that that automatically. Fixes continuous breakage for packagers.
    
    See e.g.
    
    * 4dcf6e9ce87e83cef4c2fc7483df21c684e8fff7
    * 077be5e81a092165f1c8dfeb21c28fb72644d746
    * 89ed0ff29a5eb7272bfaf66fb53de94c7d0c5f06
    * e945d755903728660d051a52bf69ad089b92f11c
    * 8277f88ca420c7e6be09203d8f6d779c4b3be5dc
    * 9ca9a69ead030dfe00690305f4d8c6872d839363
    * ee0c38c57b9a233c3669c35da9fa6b4d950c391b

--- testfiles/src/geom-predicates.h
+++ testfiles/src/geom-predicates.h
@@ -4,26 +4,25 @@
 
 #include <cmath>
 #include <gtest/gtest.h>
-#include <rect.h>
+#include <2geom/rect.h>
 
-namespace {
-
-::testing::AssertionResult RectNear(char const* expr1,
-                                    char const* expr2,
-                                    char const*,
-                                    Geom::Rect val1,
-                                    Geom::Rect val2,
-                                    double abs_error = 0.01) {
-
+inline ::testing::AssertionResult RectNear(char const *expr1,
+                                           char const *expr2,
+                                           char const *,
+                                           Geom::Rect const &val1,
+                                           Geom::Rect const &val2,
+                                           double abs_error = 0.01)
+{
     double diff = 0;
     for (auto x = 0; x < 2; x++) {
         for (auto y = 0; y < 2; y++) {
-            diff += fabs(val1[x][y] - val2[x][y]);
+            diff += std::abs(val1[x][y] - val2[x][y]);
         }
     }
 
-    if (diff <= abs_error)
+    if (diff <= abs_error) {
         return ::testing::AssertionSuccess();
+    }
 
     return ::testing::AssertionFailure()
         << "The difference between " << expr1 << " and " << expr2
@@ -33,6 +32,5 @@ namespace {
 }
 
 #define EXPECT_RECT_NEAR(a, b, eps) EXPECT_PRED_FORMAT3(RectNear, a, b, eps)
-} // namespace
 
 #endif
