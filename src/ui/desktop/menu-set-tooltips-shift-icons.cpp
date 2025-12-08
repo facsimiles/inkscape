59fb215b9fb801097c2ec6de430c0f3fdb4e51c9
PBS <pbs3141@gmail.com>
2025-12-08 17:02:29 +0100

    Bring back icons in menus
    Resurrect the commented-out code that previously showed the icons,
    and make it work with GTK4. This is a horrible hack.

--- src/ui/desktop/menu-set-tooltips-shift-icons.cpp
+++ src/ui/desktop/menu-set-tooltips-shift-icons.cpp
@@ -19,25 +19,25 @@
 
 #include "ui/desktop/menu-set-tooltips-shift-icons.h"
 
+#include <string_view>
+using namespace std::literals;
+
+#include <gtkmm/box.h>
+#include <gtkmm/image.h>
 #include <gtkmm/label.h>
 
 #include "inkscape-application.h"  // Action extra data
 #include "ui/util.h"
 
-// Could be used to update status bar.
-// bool on_enter_notify(GdkEventCrossing* crossing_event, Gtk::MenuItem* menuitem)
-// {
-//     return false;
-// }
-
-[[nodiscard]] static Glib::ustring find_label(Gtk::Widget &parent)
+template <typename T>
+static T *find_child(Gtk::Widget &parent)
 {
     for (auto &child : Inkscape::UI::children(parent)) {
-        if (auto const label_widget = dynamic_cast<Gtk::Label const *>(&child)) {
-            return label_widget->get_label();
+        if (auto t = dynamic_cast<T *>(&child)) {
+            return t;
         }
     }
-    return {};
+    return nullptr;
 }
 
 /**
@@ -47,76 +47,47 @@
  * The CSS will apply to all menu icons but is updated as each menu is shown.
  * @returns whether icons were shifted during this or an inner recursive call
  */
-bool
-set_tooltips_and_shift_icons(Gtk::Widget &menu, bool const shift_icons)
+bool set_tooltips_and_shift_icons(Gtk::Widget &menu, bool shift_icons)
 {
-    /*
-    int width{}, height{};
-
-    if (shift_icons) {
-        menu.add_css_class("shifticonmenu");
-        gtk_icon_size_lookup(GTK_ICON_SIZE_MENU, &width, &height);
-    }
-    */
-
     bool shifted = false;
 
-    // Calculate required shift. We need an example!
-    // Search for GtkModelButton -> Gtk::Box, Gtk::Image (optional), Gtk::Label.
-    static auto app = InkscapeApplication::instance();
+    auto app = InkscapeApplication::instance();
     auto &label_to_tooltip_map = app->get_menu_label_to_tooltip_map();
 
     for (auto &child : Inkscape::UI::children(menu)) {
+        if (gtk_widget_get_name(child.gobj()) == "GtkModelButton"sv) {
 
-        // Set tooltip.
-        Glib::ustring label;
-        if (child.get_name().raw() == "GtkModelButton") {
-            label = find_label(child);
-        }
+            // The ModelButton contains in order: GtkBox, GtkImage (optionally), GtkLabel, GtkPopoverMenu (optionally).
 
-        if (!label.empty()) {
-            auto it = label_to_tooltip_map.find(label);
-            if (it != label_to_tooltip_map.end()) {
-                child.set_tooltip_text(it->second); // Set tooltip on GtkModelButton.
+            // Set tooltip on GtkModelButton.
+            if (auto label_widget = find_child<Gtk::Label>(child)) {
+                auto label = label_widget->get_label();
+                if (!label.empty()) {
+                    auto it = label_to_tooltip_map.find(label);
+                    if (it != label_to_tooltip_map.end()) {
+                        child.set_tooltip_text(it->second);
+                    }
+                }
+            }
+
+            // Make GtkImage visible and move to start of GtkBox.
+            if (auto image = find_child<Gtk::Image>(child)) {
+                if (auto box = find_child<Gtk::Box>(child)) {
+                    image->reference();
+                    image->unparent();
+                    image->insert_at_start(*box);
+                    image->unreference();
+                    image->set_margin_end(5);
+                    image->set_visible();
+                    image->property_visible().signal_changed().connect([image] {
+                        if (!image->get_visible()) {
+                            image->set_visible();
+                        }
+                    });
+                }
             }
         }
 
-        // The ModelButton contains in order: GtkBox, GtkImage (optionally), GtkLabel, GtkPopoverMenu (optionally).
-        // for (auto child2 = child.get_first_child(); child2 != nullptr; child2 = child2->get_next_sibling()) {
-        //     if (auto box = dynamic_cast<Gtk::Box*>(child2)) {
-        //         // Do something with box. We might be able to use box width to calculate shift.
-        //     }
-        //     if (auto image = dynamic_cast<Gtk::Image*>(child2)) {
-        //         // Do something with image.
-        //     }
-        //     if (auto label = dynamic_cast<Gtk::Image*>(child2)) {
-        //         // Do something with label. Redundant with above.
-        //     }
-        //     if (child2->get_name() == "GtkPopoverMenu") {
-        //         // Doesn't work as there are whole lot of widgets in the tree.
-        //         set_tooltips_and_shift_icons(*child2, shift_icons);
-        //     }
-        // }
-
-        // TODO: GTK4: Figure out the kind of shift we need.
-
-        // if (!shift_icons || shifted || !box) {
-        //     return Inkscape::UI::ForEachResult::_continue;
-        // }
-
-        // width += box->get_spacing();
-        // auto const margin_side = widget->get_direction() == Gtk::TextDirection::RTL ? "right" : "left";
-        // // TODO: GTK4: as per TODOs above.
-        // auto const css_str = Glib::ustring::compose(".shifticonmenu menuitem box { margin-%1: -%2px; }"
-        //                                             ".shifticonmenu modelbutton box > label:only-child { margin-%1: %2px; }",
-        //                                             margin_side, width);
-        // Glib::RefPtr<Gtk::CssProvider> provider = Gtk::CssProvider::create();
-        // provider->load_from_data(css_str);
-        // auto const display = Gdk::Display::get_default();
-        // Gtk::StyleContext::add_provider_for_display(display, provider, GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-        // shifted = true;
-
-        // Recurse
         set_tooltips_and_shift_icons(child, shift_icons);
     }
 
