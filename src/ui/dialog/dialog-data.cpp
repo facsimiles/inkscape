7c3102ab5398846bfdc88cfecf0cfabb97d81199
Marcin Floryan <5801469-mfloryan@users.noreply.gitlab.com>
2026-01-01 15:24:08 +0100

    Making the Extensions Gallery explicitly an Extension Effects Gallery

--- src/ui/dialog/dialog-data.cpp
+++ src/ui/dialog/dialog-data.cpp
@@ -26,49 +26,49 @@ std::span<const DialogData> get_dialog_data_list() {
     static std::vector<DialogData> dialog_data_list = {
         // clang-format off
     // BASICs ----------------------------------------
-    {"FillStroke",         _("_Fill and Stroke"),      INKSCAPE_ICON("dialog-fill-and-stroke"),      DialogData::Basic,          ScrollProvider::NOPROVIDE },
-    {"Objects",            _("Layers and Object_s"),   INKSCAPE_ICON("dialog-objects"),              DialogData::Basic,          ScrollProvider::PROVIDE   },
-    {"AlignDistribute",    _("_Align and Distribute"), INKSCAPE_ICON("dialog-align-and-distribute"), DialogData::Basic,          ScrollProvider::NOPROVIDE },
-    {"Transform",          _("Transfor_m"),            INKSCAPE_ICON("dialog-transform"),            DialogData::Basic,          ScrollProvider::NOPROVIDE },
-    {"ObjectProperties",   _("_Object Properties"),    INKSCAPE_ICON("dialog-object-properties"),    DialogData::Basic,          ScrollProvider::NOPROVIDE },
-    {"Export",             _("_Export"),               INKSCAPE_ICON("document-export"),             DialogData::Basic,          ScrollProvider::PROVIDE   },
-    {"Swatches",           _("S_watches"),             INKSCAPE_ICON("swatches"),                    DialogData::Basic,          ScrollProvider::PROVIDE   },
+    {"FillStroke",         _("_Fill and Stroke"),           INKSCAPE_ICON("dialog-fill-and-stroke"),      DialogData::Basic,          ScrollProvider::NOPROVIDE },
+    {"Objects",            _("Layers and Object_s"),        INKSCAPE_ICON("dialog-objects"),              DialogData::Basic,          ScrollProvider::PROVIDE   },
+    {"AlignDistribute",    _("_Align and Distribute"),      INKSCAPE_ICON("dialog-align-and-distribute"), DialogData::Basic,          ScrollProvider::NOPROVIDE },
+    {"Transform",          _("Transfor_m"),                 INKSCAPE_ICON("dialog-transform"),            DialogData::Basic,          ScrollProvider::NOPROVIDE },
+    {"ObjectProperties",   _("_Object Properties"),         INKSCAPE_ICON("dialog-object-properties"),    DialogData::Basic,          ScrollProvider::NOPROVIDE },
+    {"Export",             _("_Export"),                    INKSCAPE_ICON("document-export"),             DialogData::Basic,          ScrollProvider::PROVIDE   },
+    {"Swatches",           _("S_watches"),                  INKSCAPE_ICON("swatches"),                    DialogData::Basic,          ScrollProvider::PROVIDE   },
 
     // TEXT ------------------------------------------
-    {"Text",               _("_Text and Font"),        INKSCAPE_ICON("dialog-text-and-font"),        DialogData::Typography,     ScrollProvider::NOPROVIDE },
-    {"FontCollections",    _("_Font Collections"),     INKSCAPE_ICON("font_collections"),            DialogData::Typography,     ScrollProvider::NOPROVIDE },
+    {"Text",               _("_Text and Font"),             INKSCAPE_ICON("dialog-text-and-font"),        DialogData::Typography,     ScrollProvider::NOPROVIDE },
+    {"FontCollections",    _("_Font Collections"),          INKSCAPE_ICON("font_collections"),            DialogData::Typography,     ScrollProvider::NOPROVIDE },
 #if WITH_LIBSPELLING
-    {"Spellcheck",         _("Check Spellin_g"),       INKSCAPE_ICON("tools-check-spelling"),        DialogData::Typography,     ScrollProvider::NOPROVIDE },
+    {"Spellcheck",         _("Check Spellin_g"),            INKSCAPE_ICON("tools-check-spelling"),        DialogData::Typography,     ScrollProvider::NOPROVIDE },
 #endif
-    {"Find",               _("_Find/Replace"),         INKSCAPE_ICON("edit-find"),                   DialogData::Typography,     ScrollProvider::NOPROVIDE },
-    {"SVGFonts",           _("SVG Font Editor"),       INKSCAPE_ICON("dialog-svg-font"),             DialogData::Typography,     ScrollProvider::NOPROVIDE },
+    {"Find",               _("_Find/Replace"),              INKSCAPE_ICON("edit-find"),                   DialogData::Typography,     ScrollProvider::NOPROVIDE },
+    {"SVGFonts",           _("SVG Font Editor"),            INKSCAPE_ICON("dialog-svg-font"),             DialogData::Typography,     ScrollProvider::NOPROVIDE },
 
     // EFFECTS ---------------------------------------
-    {"LivePathEffect",     _("Path E_ffects"),         INKSCAPE_ICON("dialog-path-effects"),         DialogData::EffectsActions, ScrollProvider::NOPROVIDE },
-    {"Trace",              _("_Trace Bitmap"),         INKSCAPE_ICON("bitmap-trace"),                DialogData::EffectsActions, ScrollProvider::NOPROVIDE },
-    {"FilterGallery",      _("Filter Gallery"),        INKSCAPE_ICON("color-filters"),               DialogData::EffectsActions, ScrollProvider::NOPROVIDE },
-    {"FilterEffects",      _("Filter _Editor"),        INKSCAPE_ICON("dialog-filters"),              DialogData::EffectsActions, ScrollProvider::NOPROVIDE },
-    {"ExtensionsGallery",  _("_Extension Gallery"),    INKSCAPE_ICON("dialog-extensions"),           DialogData::EffectsActions, ScrollProvider::NOPROVIDE },
-    {"CloneTiler",         _("Tiled Clones"),          INKSCAPE_ICON("dialog-tile-clones"),          DialogData::EffectsActions, ScrollProvider::NOPROVIDE },
+    {"LivePathEffect",     _("Path E_ffects"),              INKSCAPE_ICON("dialog-path-effects"),         DialogData::EffectsActions, ScrollProvider::NOPROVIDE },
+    {"Trace",              _("_Trace Bitmap"),              INKSCAPE_ICON("bitmap-trace"),                DialogData::EffectsActions, ScrollProvider::NOPROVIDE },
+    {"FilterGallery",      _("Filter Gallery"),             INKSCAPE_ICON("color-filters"),               DialogData::EffectsActions, ScrollProvider::NOPROVIDE },
+    {"FilterEffects",      _("Filter _Editor"),             INKSCAPE_ICON("dialog-filters"),              DialogData::EffectsActions, ScrollProvider::NOPROVIDE },
+    {"ExtensionsGallery",  _("_Effect Extensions Gallery"), INKSCAPE_ICON("dialog-extensions"),           DialogData::EffectsActions, ScrollProvider::NOPROVIDE },
+    {"CloneTiler",         _("Tiled Clones"),               INKSCAPE_ICON("dialog-tile-clones"),          DialogData::EffectsActions, ScrollProvider::NOPROVIDE },
 
     // ASSETS ----------------------------------------
-    {"Symbols",            _("S_ymbols"),              INKSCAPE_ICON("symbols"),                     DialogData::Assets,         ScrollProvider::PROVIDE   },
-    {"DocumentResources",  _("_Document Resources"),   INKSCAPE_ICON("document-resources"),          DialogData::Assets,         ScrollProvider::NOPROVIDE },
+    {"Symbols",            _("S_ymbols"),                   INKSCAPE_ICON("symbols"),                     DialogData::Assets,         ScrollProvider::PROVIDE   },
+    {"DocumentResources",  _("_Document Resources"),        INKSCAPE_ICON("document-resources"),          DialogData::Assets,         ScrollProvider::NOPROVIDE },
 
     // ADVANCED --------------------------------------
-    {"Selectors",          _("_Selectors and CSS"),    INKSCAPE_ICON("dialog-selectors"),            DialogData::Advanced,       ScrollProvider::PROVIDE   },
-    {"XMLEditor",          _("_XML Editor"),           INKSCAPE_ICON("dialog-xml-editor"),           DialogData::Advanced,       ScrollProvider::NOPROVIDE },
-    {"UndoHistory",        _("Undo _History"),         INKSCAPE_ICON("edit-undo-history"),           DialogData::Advanced,       ScrollProvider::NOPROVIDE },
-    {"IconPreview",        _("Icon Preview"),          INKSCAPE_ICON("dialog-icon-preview"),         DialogData::Advanced,       ScrollProvider::NOPROVIDE },
+    {"Selectors",          _("_Selectors and CSS"),         INKSCAPE_ICON("dialog-selectors"),            DialogData::Advanced,       ScrollProvider::PROVIDE   },
+    {"XMLEditor",          _("_XML Editor"),                INKSCAPE_ICON("dialog-xml-editor"),           DialogData::Advanced,       ScrollProvider::NOPROVIDE },
+    {"UndoHistory",        _("Undo _History"),              INKSCAPE_ICON("edit-undo-history"),           DialogData::Advanced,       ScrollProvider::NOPROVIDE },
+    {"IconPreview",        _("Icon Preview"),               INKSCAPE_ICON("dialog-icon-preview"),         DialogData::Advanced,       ScrollProvider::NOPROVIDE },
 
     // SETTINGS --------------------------------------
-    {"DocumentProperties", _("_Document Properties"),  INKSCAPE_ICON("document-properties"),         DialogData::Settings,       ScrollProvider::PROVIDE   },
-    {"Preferences",        _("P_references"),          INKSCAPE_ICON("preferences-system"),          DialogData::Settings,       ScrollProvider::PROVIDE   },
+    {"DocumentProperties", _("_Document Properties"),       INKSCAPE_ICON("document-properties"),         DialogData::Settings,       ScrollProvider::PROVIDE   },
+    {"Preferences",        _("P_references"),               INKSCAPE_ICON("preferences-system"),          DialogData::Settings,       ScrollProvider::PROVIDE   },
 
     // All others (hidden) ---------------------------
-    {"DebugWindow",        _("_Debugger"),             INKSCAPE_ICON("dialog-debug"),                DialogData::Diagnostics,    ScrollProvider::NOPROVIDE },
+    {"DebugWindow",        _("_Debugger"),                  INKSCAPE_ICON("dialog-debug"),                DialogData::Diagnostics,    ScrollProvider::NOPROVIDE },
 #if DEBUG
-    {"Prototype",          _("Prototype"),             INKSCAPE_ICON("document-properties"),         DialogData::Other,          ScrollProvider::NOPROVIDE },
+    {"Prototype",          _("Prototype"),                  INKSCAPE_ICON("document-properties"),         DialogData::Other,          ScrollProvider::NOPROVIDE },
 #endif
         // clang-format on
     };
